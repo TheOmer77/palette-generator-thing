a3e54030c89fc3ead33140652337503ae855f5d8
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 19:48:11 +0300

    Make scrolling CodeBlock properly rounded

--- src/components/general/CodeBlock.tsx
+++ src/components/general/CodeBlock.tsx
@@ -4,14 +4,16 @@ const CodeBlock = forwardRef<
   HTMLPreElement,
   DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>
 >(({ children, ...props }, ref) => (
-  <pre
-    {...props}
-    ref={ref}
-    className='max-h-[calc(100vh-4rem)] overflow-auto rounded-lg bg-slate-100
-    p-4 text-sm dark:bg-slate-900'
-  >
-    {children}
-  </pre>
+  <div className='overflow-hidden rounded-lg'>
+    <pre
+      {...props}
+      ref={ref}
+      className='max-h-[calc(100vh-4rem)] overflow-auto bg-slate-100 p-4 text-sm
+    dark:bg-slate-900'
+    >
+      {children}
+    </pre>
+  </div>
 ));
 CodeBlock.displayName = 'CodeBlock';
 
