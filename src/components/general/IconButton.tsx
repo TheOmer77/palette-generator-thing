3e505ce2e6f6df33657d3d1fde45edf9c31fa9bc
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 19:41:13 +0300

    Add dark styles to IconButton

--- src/components/general/IconButton.tsx
+++ src/components/general/IconButton.tsx
@@ -14,8 +14,9 @@ const IconButton = forwardRef<
     ref={ref}
     className='flex h-8 w-8 cursor-default items-center justify-center rounded-lg
     text-xl text-slate-700 hover:bg-slate-950/20 focus-visible:bg-slate-950/20
-    focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-600
-    active:bg-slate-950/30'
+    focus-visible:outline-none active:bg-slate-950/30 dark:text-slate-400
+  dark:hover:bg-slate-50/20 dark:focus-visible:bg-slate-50/20
+  dark:active:bg-slate-50/30'
   >
     {Children.only(children)}
   </button>
