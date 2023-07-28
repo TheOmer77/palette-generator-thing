8dd536f70952f7bd9a1a35a885afc29ee7ed282c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 20:54:22 +0300

    Use `cn` utils in IconButton

--- src/components/general/IconButton.tsx
+++ src/components/general/IconButton.tsx
@@ -4,17 +4,21 @@ import {
   DetailedHTMLProps,
   forwardRef,
 } from 'react';
+import cn from 'utils/cn';
 
 const IconButton = forwardRef<
   HTMLButtonElement,
   DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
->(({ children, ...props }, ref) => (
+>(({ children, className, ...props }, ref) => (
   <button
     {...props}
     ref={ref}
-    className='flex h-8 w-8 cursor-default items-center justify-center rounded-lg
-    text-xl text-slate-700 hover:bg-slate-400/20 focus-visible:bg-slate-400/20
-    focus-visible:outline-none active:bg-slate-400/30 dark:text-slate-400'
+    className={cn(
+      `flex h-8 w-8 cursor-default items-center justify-center rounded-lg
+text-xl text-slate-700 hover:bg-slate-400/20 focus-visible:bg-slate-400/20
+focus-visible:outline-none active:bg-slate-400/30 dark:text-slate-400`,
+      className
+    )}
   >
     {Children.only(children)}
   </button>
