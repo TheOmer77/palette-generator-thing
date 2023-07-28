ae6ca382c20b2400ae3378f22c2d0aae54684f7d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 19:28:53 +0300

    Add dark styles to headings

--- src/components/general/H3.tsx
+++ src/components/general/H3.tsx
@@ -4,7 +4,11 @@ const H3 = forwardRef<
   HTMLHeadingElement,
   DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>
 >(({ children, ...props }, ref) => (
-  <h3 {...props} ref={ref} className='text-xl font-medium text-slate-700'>
+  <h3
+    {...props}
+    ref={ref}
+    className='text-xl font-medium text-slate-700 dark:text-slate-400'
+  >
     {children}
   </h3>
 ));
