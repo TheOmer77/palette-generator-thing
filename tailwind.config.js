50d4e381efafb673d1cb109ac2f4155c2c17d4c2
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 20:19:38 +0300

    Set mono font to Fira Code in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: { sans: ['var(--font-family)', 'sans-serif'] },
+      fontFamily: {
+        sans: ['var(--font-family)', 'sans-serif'],
+        mono: ['var(--font-family-mono)', 'monospace'],
+      },
     },
   },
   plugins: [],
