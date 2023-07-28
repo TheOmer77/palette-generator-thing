e99c2ebbdb2bd06d7f9b44fc6b1a512f943c16b3
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 19:41:22 +0300

    Add dark styles to Input

--- src/components/general/Input.tsx
+++ src/components/general/Input.tsx
@@ -18,14 +18,15 @@ interface InputProps
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ id, label, startAdornment, endAdornment, ...props }, ref) => (
     <div
-      className='relative mt-6 flex h-12 items-center gap-2 rounded-lg
-    bg-slate-50 px-2 ring-1 ring-slate-300 focus-within:ring-2
-    focus-within:ring-blue-600 [&>label]:focus-within:text-blue-600'
+      className='relative mt-6 flex h-12 items-center gap-2 rounded-lg bg-slate-50
+      px-2 ring-1 ring-slate-300 focus-within:ring-2 focus-within:ring-blue-600
+    dark:bg-slate-950 dark:ring-slate-700 dark:focus-within:ring-blue-300
+    [&>label]:focus-within:text-blue-600 dark:[&>label]:focus-within:text-blue-300'
     >
       <label
         htmlFor={id}
         className='absolute top-[-1.5rem] select-none text-sm font-medium
-      text-slate-600'
+      text-slate-600 dark:text-slate-400'
       >
         {label}
       </label>
@@ -34,8 +35,8 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         {...props}
         ref={ref}
         id={id}
-        className='flex-grow bg-transparent text-slate-900
-        focus-visible:outline-none'
+        className='flex-grow bg-transparent text-slate-900 focus-visible:outline-none
+        dark:text-slate-100'
       />
       {endAdornment}
     </div>
