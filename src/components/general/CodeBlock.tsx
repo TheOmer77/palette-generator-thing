07c6e1725498152cb28314ba9fb76b878313d658
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 20:55:49 +0300

    Add copy code button to CodeBlock

--- src/components/general/CodeBlock.tsx
+++ src/components/general/CodeBlock.tsx
@@ -1,20 +1,56 @@
-import { DetailedHTMLProps, HTMLAttributes, forwardRef } from 'react';
+import {
+  DetailedHTMLProps,
+  HTMLAttributes,
+  forwardRef,
+  useCallback,
+  useState,
+} from 'react';
 
-const CodeBlock = forwardRef<
-  HTMLPreElement,
-  DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>
->(({ children, ...props }, ref) => (
-  <div className='overflow-hidden rounded-lg'>
-    <pre
-      {...props}
-      ref={ref}
-      className='max-h-[calc(100vh-4rem)] overflow-auto bg-slate-100 p-4 text-sm
+import { IconButton } from 'components/general';
+import { ReactComponent as CopyIcon } from 'assets/icons/copy.svg';
+import { ReactComponent as DoneIcon } from 'assets/icons/done.svg';
+
+interface CodeBlockProps
+  extends Omit<
+    DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>,
+    'children'
+  > {
+  children: string;
+}
+
+const CodeBlock = forwardRef<HTMLPreElement, CodeBlockProps>(
+  ({ children, ...props }, ref) => {
+    const [justCopied, setJustCopied] = useState(false);
+
+    const copyCode = useCallback(() => {
+      navigator.clipboard.writeText(children);
+      setJustCopied(true);
+      setTimeout(() => setJustCopied(false), 1500);
+    }, [children]);
+
+    return (
+      <div
+        className='relative overflow-hidden rounded-lg bg-slate-100
     dark:bg-slate-900/40'
-    >
-      {children}
-    </pre>
-  </div>
-));
+      >
+        <IconButton
+          title='Copy code'
+          className='absolute end-2 top-2'
+          onClick={copyCode}
+        >
+          {justCopied ? <DoneIcon /> : <CopyIcon />}
+        </IconButton>
+        <pre
+          {...props}
+          ref={ref}
+          className='max-h-[calc(100vh-4rem)] overflow-auto p-4 text-sm'
+        >
+          {children}
+        </pre>
+      </div>
+    );
+  }
+);
 CodeBlock.displayName = 'CodeBlock';
 
 export default CodeBlock;
