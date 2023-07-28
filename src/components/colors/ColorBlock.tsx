73e9fe113de0f4eede11b73058fc2119e2d0b267
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 21:41:12 +0300

    Add hover-only copy button to ColorBlock

--- src/components/colors/ColorBlock.tsx
+++ src/components/colors/ColorBlock.tsx
@@ -1,5 +1,8 @@
-import { forwardRef } from 'react';
+import { forwardRef, useCallback, useState } from 'react';
+import { IconButton } from 'components/general';
 import { hexInverseBw, isValidHexColor } from 'utils/colorUtils';
+import { ReactComponent as CopyIcon } from 'assets/icons/copy.svg';
+import { ReactComponent as DoneIcon } from 'assets/icons/done.svg';
 
 interface ColorBlockProps
   extends React.DetailedHTMLProps<
@@ -10,21 +13,38 @@ interface ColorBlockProps
 }
 
 const ColorBlock = forwardRef<HTMLDivElement, ColorBlockProps>(
-  ({ value, style, ...props }, ref) => (
-    <div
-      {...props}
-      ref={ref}
-      style={{
-        ...(isValidHexColor(value)
-          ? { backgroundColor: value, color: hexInverseBw(value) }
-          : {}),
-        ...style,
-      }}
-      className='flex flex-col items-start justify-center rounded-lg p-2'
-    >
-      {value}
-    </div>
-  )
+  ({ value, style, ...props }, ref) => {
+    const [justCopied, setJustCopied] = useState(false);
+
+    const copyValue = useCallback(() => {
+      navigator.clipboard.writeText(value);
+      setJustCopied(true);
+      setTimeout(() => setJustCopied(false), 1500);
+    }, [value]);
+
+    return (
+      <div
+        {...props}
+        ref={ref}
+        style={{
+          ...(isValidHexColor(value)
+            ? { backgroundColor: value, color: hexInverseBw(value) }
+            : {}),
+          ...style,
+        }}
+        className='relative flex flex-col items-start justify-center rounded-lg p-2 [&>button]:hover:flex'
+      >
+        <IconButton
+          title='Copy color value'
+          className='absolute end-2 top-1 hidden text-inherit dark:text-inherit'
+          onClick={copyValue}
+        >
+          {justCopied ? <DoneIcon /> : <CopyIcon />}
+        </IconButton>
+        {value}
+      </div>
+    );
+  }
 );
 ColorBlock.displayName = 'ColorBlock';
 
