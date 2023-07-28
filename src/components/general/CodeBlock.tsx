67def000fca498a1129c2e5f09f3b19b224602d8
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 20:12:35 +0300

    Tweak CodeBlock dark BG

--- src/components/general/CodeBlock.tsx
+++ src/components/general/CodeBlock.tsx
@@ -9,7 +9,7 @@ const CodeBlock = forwardRef<
       {...props}
       ref={ref}
       className='max-h-[calc(100vh-4rem)] overflow-auto bg-slate-100 p-4 text-sm
-    dark:bg-slate-900'
+    dark:bg-slate-900/40'
     >
       {children}
     </pre>
