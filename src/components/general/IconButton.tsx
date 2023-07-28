44e0e89636b5928c1ab0872d28460447965925f2
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-07-28 20:24:18 +0300

    Tweak IconButton hover BG

--- src/components/general/IconButton.tsx
+++ src/components/general/IconButton.tsx
@@ -13,10 +13,8 @@ const IconButton = forwardRef<
     {...props}
     ref={ref}
     className='flex h-8 w-8 cursor-default items-center justify-center rounded-lg
-    text-xl text-slate-700 hover:bg-slate-950/20 focus-visible:bg-slate-950/20
-    focus-visible:outline-none active:bg-slate-950/30 dark:text-slate-400
-  dark:hover:bg-slate-50/20 dark:focus-visible:bg-slate-50/20
-  dark:active:bg-slate-50/30'
+    text-xl text-slate-700 hover:bg-slate-400/20 focus-visible:bg-slate-400/20
+    focus-visible:outline-none active:bg-slate-400/30 dark:text-slate-400'
   >
     {Children.only(children)}
   </button>
