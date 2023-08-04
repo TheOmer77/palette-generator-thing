b3f85aa8fd71efc1b475dd92fab427cdaeba065e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-04 22:02:12 +0300

    Add medium font weight to Button

--- src/components/general/Button.tsx
+++ src/components/general/Button.tsx
@@ -8,9 +8,9 @@ const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
       ref={ref}
       className={cn(
         `flex cursor-default select-none items-center justify-center self-center
-rounded-lg px-3 py-1.5 text-base text-slate-700 hover:bg-slate-500/20
-focus-visible:bg-slate-500/20 focus-visible:outline-none active:bg-slate-500/30
-dark:text-slate-300`,
+rounded-lg px-3 py-1.5 text-base font-medium text-slate-700
+hover:bg-slate-500/20 focus-visible:bg-slate-500/20 focus-visible:outline-none
+active:bg-slate-500/30 dark:text-slate-300`,
         className
       )}
     >
