d889eab97dcc10dccc5b41761ea7ab0fb4167340
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-05 01:40:36 +0300

    Render sidebar as a floating bar on mobile

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -11,8 +11,9 @@ const Sidebar = () => {
 
   return (
     <aside
-      className='fixed inset-x-0 bottom-0 z-10 flex flex-col gap-4 bg-slate-200
-    p-2 dark:bg-slate-900 md:inset-x-auto md:h-screen md:w-[25rem]'
+      className='fixed bottom-2 start-1/2 z-10 flex -translate-x-1/2 flex-col
+gap-4 rounded-lg bg-slate-200 p-2 dark:bg-slate-900 md:inset-x-auto
+md:bottom-0 md:h-screen md:w-[25rem] md:translate-x-0 md:rounded-none'
     >
       <Header className='hidden md:block' />
 
