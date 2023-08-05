f90054315229b0379b8afd1468795c8da7545ce6
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-05 22:00:24 +0300

    Tweak mobile options button style

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -10,25 +10,32 @@ const Sidebar = () => {
   const mdBreakpoint = useTailwindBreakpoint('md');
   const [sheetOpen, setSheetOpen] = useState(false);
 
-  return (
+  return mdBreakpoint ? (
     <aside
-      className='fixed bottom-2 start-1/2 z-10 flex -translate-x-1/2 flex-col
-gap-4 rounded-lg bg-slate-200 p-2 dark:bg-slate-900 md:inset-x-auto
-md:bottom-0 md:h-screen md:w-[25rem] md:translate-x-0 md:rounded-none'
+      className='fixed flex h-screen
+w-[25rem] flex-col gap-4 rounded-lg
+bg-slate-200 p-2 dark:bg-slate-900 md:rounded-none'
     >
       <Header className='hidden md:block' />
-
-      {mdBreakpoint ? (
-        <OptionsSection />
-      ) : (
-        <Button icon={<TuneIcon />} onClick={() => setSheetOpen(true)}>
-          Options
-        </Button>
-      )}
+      <OptionsSection />
+    </aside>
+  ) : (
+    <>
+      <Button
+        icon={<TuneIcon />}
+        onClick={() => setSheetOpen(true)}
+        className='fixed bottom-4 end-4 z-10 h-12
+bg-slate-200 px-4 shadow shadow-slate-900/50 hover:bg-slate-300
+focus-visible:bg-slate-300 focus-visible:outline-none active:bg-slate-400
+dark:bg-slate-900 dark:shadow-slate-700/50 dark:hover:bg-slate-800
+dark:focus-visible:bg-slate-800 dark:active:bg-slate-700'
+      >
+        Options
+      </Button>
       <BottomSheet open={sheetOpen} onOpenChange={setSheetOpen} title='Options'>
         <OptionsSection />
       </BottomSheet>
-    </aside>
+    </>
   );
 };
 
