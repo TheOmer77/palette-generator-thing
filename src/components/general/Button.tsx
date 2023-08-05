0555f0233f0fab699a3ec489982914f24ce78501
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-05 19:50:26 +0300

    Tweak Button height

--- src/components/general/Button.tsx
+++ src/components/general/Button.tsx
@@ -7,8 +7,8 @@ const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(
       {...props}
       ref={ref}
       className={cn(
-        `flex cursor-default select-none items-center justify-center self-center
-rounded-lg px-3 py-1.5 text-base font-medium text-slate-700
+        `flex h-9 cursor-default select-none items-center justify-center 
+self-center rounded-lg px-3 text-base font-medium text-slate-700 
 hover:bg-slate-500/20 focus-visible:bg-slate-500/20 focus-visible:outline-none
 active:bg-slate-500/30 dark:text-slate-300`,
         className
