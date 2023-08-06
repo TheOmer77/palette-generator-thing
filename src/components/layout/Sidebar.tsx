36aa7e463ec2fa66b4fd73d87070a24591a641af
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-06 19:51:46 +0300

    Use FAB as Sidebar mobile options button

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { BottomSheet, Button } from 'components/general';
+import { BottomSheet, Fab } from 'components/general';
 import Header from './Header';
 import OptionsSection from './OptionsSection';
 import useTailwindBreakpoint from 'hooks/useTailwindBreakpoint';
@@ -21,19 +21,9 @@ bg-slate-200 p-2 dark:bg-slate-900 md:rounded-none'
     </aside>
   ) : (
     <>
-      <Button
-        icon={<TuneIcon />}
-        onClick={() => setSheetOpen(true)}
-        className='fixed bottom-4 end-4 z-10 h-12
-overflow-hidden bg-slate-200 px-4 shadow shadow-slate-500/50 after:absolute
-after:start-0 after:top-0 after:h-full after:w-full after:content-[""]
-hover:bg-slate-200 hover:after:bg-slate-500/10 focus-visible:bg-slate-300
-focus-visible:outline-none active:bg-slate-300 dark:bg-slate-900
-dark:shadow-slate-700/50 hover:dark:bg-slate-900 
-dark:focus-visible:bg-slate-800 dark:active:bg-slate-800'
-      >
+      <Fab icon={<TuneIcon />} onClick={() => setSheetOpen(true)}>
         Options
-      </Button>
+      </Fab>
       <BottomSheet open={sheetOpen} onOpenChange={setSheetOpen} title='Options'>
         <OptionsSection />
       </BottomSheet>
