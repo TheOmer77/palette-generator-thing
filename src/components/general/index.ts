39afda5f40e34186971e79b4ebfd1f94d5aa0cbe
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-06 19:50:18 +0300

    Add FAB component

--- src/components/general/index.ts
+++ src/components/general/index.ts
@@ -1,6 +1,7 @@
 export { default as BottomSheet, type BottomSheetProps } from './BottomSheet';
 export { default as Button, type ButtonProps } from './Button';
 export { default as CodeBlock, type CodeBlockProps } from './CodeBlock';
+export { default as Fab } from './Fab';
 export { default as H2 } from './H2';
 export { default as H3 } from './H3';
 export { default as IconButton } from './IconButton';
