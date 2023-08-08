8c3f0aa545901faf960c0fa85ba56a9545ceb42a
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-08 21:43:33 +0300

    Make FAB larger

--- src/components/general/Fab.tsx
+++ src/components/general/Fab.tsx
@@ -8,15 +8,15 @@ const Fab = forwardRef<HTMLButtonElement, ButtonProps>(
       {...props}
       ref={ref}
       className={cn(
-        `fixed bottom-4 end-4 z-10 flex h-12 cursor-default select-none
-items-center justify-center gap-2 self-center overflow-hidden rounded-lg
+        `fixed bottom-4 end-4 z-10 flex h-14 cursor-default select-none
+items-center justify-center gap-3 self-center overflow-hidden rounded-lg
 bg-slate-200 text-base font-medium text-slate-700 shadow
 shadow-slate-500/50 after:absolute after:start-0 after:top-0 after:h-full
 after:w-full after:content-[""] hover:after:bg-slate-500/10
 focus-visible:bg-slate-300 focus-visible:outline-none active:bg-slate-300
 dark:bg-slate-900 dark:text-slate-300 dark:shadow-slate-700/50
-dark:focus-visible:bg-slate-800 dark:active:bg-slate-800 [&>.icon]:text-xl`,
-        isValidElement(icon) && !children ? 'w-12' : 'px-4',
+dark:focus-visible:bg-slate-800 dark:active:bg-slate-800 [&>.icon]:text-2xl`,
+        isValidElement(icon) && !children ? 'w-14' : 'px-4',
         className
       )}
     >
