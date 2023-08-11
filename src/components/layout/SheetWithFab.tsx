4a830d009f6634d5314d0be27a9c518d8f231f7b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-11 15:16:48 +0300

    Add SheetWithFab component

---
+++ src/components/layout/SheetWithFab.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, useState } from 'react';
+import { BottomSheet, Fab } from 'components/general';
+
+export interface SheetWithFabProps {
+  label: string;
+  fabIcon?: ReactNode;
+  children?: ReactNode;
+}
+
+const SheetWithFab = ({ label, fabIcon, children }: SheetWithFabProps) => {
+  const [sheetOpen, setSheetOpen] = useState(false);
+
+  return (
+    <>
+      <Fab icon={fabIcon} onClick={() => setSheetOpen(true)}>
+        {label}
+      </Fab>
+      <BottomSheet title={label} open={sheetOpen} onOpenChange={setSheetOpen}>
+        {children}
+      </BottomSheet>
+    </>
+  );
+};
+
+export default SheetWithFab;
