5204348f6a8469b0cc94def7a8497253e9b0506b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-12 12:35:20 +0300

    Add `useTheme` hook with all theme base colors

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import useGlobalState from './useGlobalState';
+import {
+  getErrorColorHex,
+  getNeutralVariantHex,
+  getSecondaryColorHex,
+} from 'utils/colorUtils';
+
+const useTheme = () => {
+  const [{ baseColor }] = useGlobalState();
+
+  const neutralColor = useMemo(
+      () => getNeutralVariantHex(baseColor),
+      [baseColor]
+    ),
+    secondaryColor = useMemo(
+      () => getSecondaryColorHex(baseColor),
+      [baseColor]
+    ),
+    errorColor = useMemo(() => getErrorColorHex(baseColor), [baseColor]);
+
+  return [baseColor, neutralColor, secondaryColor, errorColor];
+};
+
+export default useTheme;
