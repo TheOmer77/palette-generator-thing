6cefdb27f7c23c145b51de9a45e069cef0f2a4f8
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-26 16:07:06 +0300

    Reduce number of tones/shades to 11

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,9 @@
-export const tones = [...[...Array(19).keys()].map(key => key * 5 + 5), 98];
+export const shades = [
+  50,
+  ...[...Array(9).keys()].map(key => (key + 1) * 100),
+  950,
+];
+export const tones = shades.map(shade => shade / 10);
 export const defaultErrorHue = 25;
 export * as prismThemes from './prism';
 export { default as screens } from './breakpoints';
