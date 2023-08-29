bab2dc41bf5a270a01c0ba694905df5658723e6d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-29 16:46:18 +0300

    Fix CSS variables style missing `:root`

--- src/utils/generateVariablesCss.ts
+++ src/utils/generateVariablesCss.ts
@@ -1,20 +1,21 @@
 import { shades } from 'constants';
 import { generatePalette } from './colorUtils';
 
-const generateVariablesCss = (baseColors: { [colorName: string]: string }) => {
-  return Object.keys(baseColors)
+const generateVariablesCss = (baseColors: { [colorName: string]: string }) =>
+  `:root {
+  ${Object.keys(baseColors)
     .map(
       (key: keyof typeof baseColors) => `/* ${key} */
 ${generatePalette(baseColors[key], 'rgbValues')
   .map(
     (rgbValues, index) =>
-      `--color-${key}-${shades[index]}: ${rgbValues.join(' ')};`
+      `  --color-${key}-${shades[index]}: ${rgbValues.join(' ')};`
   )
   .join('\n')}`
     )
-    .join('\n\n');
+    .join('\n\n')}
+}`;
 
-  // TODO: Find main, light, dark, contrast values
-};
+// TODO: Find main, light, dark, contrast values
 
 export default generateVariablesCss;
