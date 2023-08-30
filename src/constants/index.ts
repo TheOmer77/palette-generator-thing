947050445e78c0b16952d0afa9400f1bd0bb1c34
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-08-30 17:51:06 +0300

    Remove unused tones const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,6 @@ export const shades = [
   ...[...Array(9).keys()].map(key => (key + 1) * 100),
   950,
 ];
-export const tones = shades.map(shade => shade / 10);
 export const defaultErrorHue = 25;
 export * as prismThemes from './prism';
 export { default as screens } from './breakpoints';
