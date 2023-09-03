1ecb37f2857c2055cd58f81f78532dd448ff728e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-03 16:45:20 +0300

    Fix input autocomplete blue background

--- src/components/general/Input.tsx
+++ src/components/general/Input.tsx
@@ -31,8 +31,9 @@ text-neutral-600 dark:text-neutral-400'
         {...props}
         ref={ref}
         id={id}
-        className='flex-grow bg-transparent text-neutral-900
-focus-visible:outline-none dark:text-neutral-100'
+        className='autocomplete-override-white
+dark:autocomplete-override-neutral-950 flex-grow bg-transparent
+text-neutral-900 focus-visible:outline-none dark:text-neutral-100'
       />
       {endAdornment}
     </div>
