2e3a27800f6d481291992fc4ab8a8475bd93ba68
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-05 21:57:22 +0300

    Add `useMemo` back in useTheme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,12 +1,13 @@
+import { useMemo } from 'react';
 import useGlobalState from './useGlobalState';
 import { getDangerColor, getNeutralColor, getSecondaryColor } from 'utils';
 
 const useTheme = () => {
   const [{ baseColor }] = useGlobalState();
 
-  const neutralColor = getNeutralColor(baseColor);
-  const secondaryColor = getSecondaryColor(baseColor);
-  const dangerColor = getDangerColor(baseColor);
+  const neutralColor = useMemo(() => getNeutralColor(baseColor), [baseColor]),
+    secondaryColor = useMemo(() => getSecondaryColor(baseColor), [baseColor]),
+    dangerColor = useMemo(() => getDangerColor(baseColor), [baseColor]);
 
   return [baseColor, neutralColor, secondaryColor, dangerColor];
 };
