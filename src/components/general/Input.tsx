63801016104477789736039f1092fa4cfdbdd189
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-08 13:46:12 +0300

    Add `asChild` prop to Input
    This allows using a component other than <input> as the base input
    component.

--- src/components/general/Input.tsx
+++ src/components/general/Input.tsx
@@ -1,43 +1,60 @@
-import { forwardRef, ReactNode, ComponentProps } from 'react';
+import { forwardRef, type ComponentProps, type ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
 import { cn } from 'utils';
 
 export interface InputProps extends ComponentProps<'input'> {
   label?: string;
   startAdornment?: ReactNode;
   endAdornment?: ReactNode;
+  asChild?: boolean;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ id, label, startAdornment, endAdornment, className, ...props }, ref) => (
-    <div
-      className={cn(
-        `relative mt-6 flex h-12 items-center gap-2 rounded-lg bg-white px-2
+  (
+    {
+      id,
+      label,
+      startAdornment,
+      endAdornment,
+      asChild = false,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const InputComponent = asChild ? Slot : 'input';
+    return (
+      <div
+        className={cn(
+          `relative mt-6 flex h-12 items-center gap-2 rounded-lg bg-white px-2
 ring-1 ring-neutral-300 focus-within:ring-2 focus-within:ring-primary-500
 dark:bg-neutral-950 dark:ring-neutral-700 dark:focus-within:ring-primary-300
 [&>label]:focus-within:text-primary-500
 dark:[&>label]:focus-within:text-primary-300`,
-        className
-      )}
-    >
-      <label
-        htmlFor={id}
-        className='absolute -top-6 select-none text-sm font-medium
-text-neutral-600 dark:text-neutral-400'
+          className
+        )}
       >
-        {label}
-      </label>
-      {startAdornment}
-      <input
-        {...props}
-        ref={ref}
-        id={id}
-        className='flex-grow bg-transparent text-neutral-900
+        <label
+          htmlFor={id}
+          className='absolute -top-6 select-none text-sm font-medium
+text-neutral-600 dark:text-neutral-400'
+        >
+          {label}
+        </label>
+        {startAdornment}
+        <InputComponent
+          {...props}
+          ref={ref}
+          id={id}
+          className='flex-grow bg-transparent text-neutral-900
 autofill-override-white focus-visible:outline-none dark:text-neutral-100
 dark:autofill-override-neutral-950'
-      />
-      {endAdornment}
-    </div>
-  )
+        />
+        {endAdornment}
+      </div>
+    );
+  }
 );
 Input.displayName = 'Input';
 
