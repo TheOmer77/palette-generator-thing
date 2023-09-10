894725ddca7fe202772dc7d4a80d82a4b63e30c1
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-10 16:37:00 +0300

    Add roving focus & disabled prop to List

--- src/components/general/List/List.tsx
+++ src/components/general/List/List.tsx
@@ -1,18 +1,70 @@
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 import { cn } from 'utils';
 
-export const List = forwardRef<
-  HTMLUListElement,
-  ComponentPropsWithoutRef<'ul'>
->(({ children, className, ...props }, ref) => {
-  return (
-    <ul
-      ref={ref}
-      className={cn(`flex w-full flex-col space-y-px rounded-lg`, className)}
-      {...props}
-    >
-      {children}
-    </ul>
-  );
-});
-List.displayName = 'List';
+import { RovingFocusGroup } from '@radix-ui/react-roving-focus';
+
+import {
+  LIST_GROUP_NAME,
+  ListContext,
+  ScopedProps,
+  useRovingFocusGroupScope,
+} from './common';
+
+type RovingFocusGroupProps = ComponentPropsWithoutRef<typeof RovingFocusGroup>;
+
+interface ListProps extends ComponentPropsWithoutRef<'ul'> {
+  disabled?: boolean;
+  rovingFocus?: boolean;
+  loop?: RovingFocusGroupProps['loop'];
+  dir?: RovingFocusGroupProps['dir'];
+}
+
+const listClassName = `flex w-full flex-col space-y-px rounded-lg`;
+
+export const List = forwardRef<HTMLUListElement, ScopedProps<ListProps>>(
+  (
+    {
+      __scopeToggleGroup,
+      disabled = false,
+      rovingFocus = true,
+      loop = false,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const rovingFocusGroupScope = useRovingFocusGroupScope(__scopeToggleGroup);
+
+    return (
+      <ListContext
+        scope={__scopeToggleGroup}
+        rovingFocus={rovingFocus}
+        disabled={disabled}
+      >
+        {rovingFocus ? (
+          <RovingFocusGroup
+            asChild
+            {...rovingFocusGroupScope}
+            orientation='vertical'
+            loop={loop}
+          >
+            <ul
+              {...props}
+              ref={ref}
+              role='group'
+              className={cn(listClassName, className)}
+            />
+          </RovingFocusGroup>
+        ) : (
+          <ul
+            {...props}
+            ref={ref}
+            role='group'
+            className={cn(listClassName, className)}
+          />
+        )}
+      </ListContext>
+    );
+  }
+);
+List.displayName = LIST_GROUP_NAME;
--- src/components/general/List/ListItem.tsx
+++ src/components/general/List/ListItem.tsx
@@ -1,25 +1,57 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react';
+import { ComponentPropsWithoutRef, forwardRef, useRef } from 'react';
+import { RovingFocusGroupItem } from '@radix-ui/react-roving-focus';
+
 import { cn } from 'utils';
 
-export const ListItem = forwardRef<
-  HTMLButtonElement,
-  ComponentPropsWithoutRef<'button'>
->(({ children, className, ...props }, ref) => (
-  <li className='p-0'>
-    <button
-      ref={ref}
-      className={cn(
-        `relative flex h-12 w-full cursor-default select-none items-center p-4
+const listItemClassName = `relative flex h-12 w-full cursor-default select-none items-center p-4
 outline-none transition-[background-color] after:absolute after:start-0
 after:top-0 after:h-full after:w-full after:rounded-lg after:content-[""]
 hover:after:bg-neutral-500/20 focus-visible:outline-none
-focus-visible:after:bg-neutral-500/20 active:after:bg-neutral-500/30`,
-        className
+focus-visible:after:bg-neutral-500/20 active:after:bg-neutral-500/30`;
+
+import {
+  LIST_ITEM_NAME,
+  ScopedProps,
+  useListContext,
+  useRovingFocusGroupScope,
+} from './common';
+
+export interface ListItemProps extends ComponentPropsWithoutRef<'button'> {}
+
+export const ListItem = forwardRef<
+  HTMLButtonElement,
+  ScopedProps<ListItemProps>
+>(({ __scopeToggleGroup, className, ...props }, ref) => {
+  const context = useListContext(LIST_ITEM_NAME, __scopeToggleGroup);
+  const rovingFocusGroupScope = useRovingFocusGroupScope(__scopeToggleGroup);
+  const disabled = context.disabled || props.disabled;
+  const rovingFocusItemRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <li className='p-0'>
+      {context.rovingFocus ? (
+        <RovingFocusGroupItem
+          asChild
+          {...rovingFocusGroupScope}
+          focusable={!disabled}
+          ref={rovingFocusItemRef}
+        >
+          <button
+            {...props}
+            ref={ref}
+            disabled={disabled}
+            className={cn(listItemClassName, className)}
+          />
+        </RovingFocusGroupItem>
+      ) : (
+        <button
+          {...props}
+          ref={ref}
+          disabled={disabled}
+          className={cn(listItemClassName, className)}
+        />
       )}
-      {...props}
-    >
-      {children}
-    </button>
-  </li>
-));
-ListItem.displayName = 'ListItem';
+    </li>
+  );
+});
+ListItem.displayName = LIST_ITEM_NAME;
