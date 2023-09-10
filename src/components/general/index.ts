af88415989f7e9536c02d6219931b249d380baba
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-10 22:56:23 +0300

    Add Collapsible component
    Content in this component can be collapsed or expanded with an animation

--- src/components/general/index.ts
+++ src/components/general/index.ts
@@ -6,4 +6,5 @@ export { default as H2 } from './H2';
 export { default as H3 } from './H3';
 export { default as IconButton } from './IconButton';
 export { default as Input, type InputProps } from './Input';
+export * from './Collapsible';
 export * from './List';
