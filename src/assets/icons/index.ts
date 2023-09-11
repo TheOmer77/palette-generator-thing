8a9aba101d349dd09a43514dcc9baf1ede6dc8a9
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-11 13:58:56 +0300

    Add expand more icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as CopyIcon } from './copy.svg';
 export { ReactComponent as DoneIcon } from './done.svg';
 export { ReactComponent as RandomIcon } from './random.svg';
 export { ReactComponent as TuneIcon } from './tune.svg';
+export { ReactComponent as ExpandMoreIcon } from './expandMore.svg';
