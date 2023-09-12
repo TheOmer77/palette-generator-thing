3726249a10ebafc7433cfcaa7d120ae156c60df3
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-13 00:05:12 +0300

    Update styles & add disabled styles to Radio

--- src/components/general/Radio/index.tsx
+++ src/components/general/Radio/index.tsx
@@ -8,20 +8,24 @@ export interface RadioProps
 }
 
 export const Radio = forwardRef<HTMLButtonElement, RadioProps>(
-  ({ checked = false, className, ...props }, ref) => (
+  ({ checked = false, disabled, className, ...props }, ref) => (
     <Primitive.button
       {...props}
       ref={ref}
+      disabled={disabled}
       data-state={checked ? 'checked' : 'unchecked'}
+      aria-disabled={disabled}
       className={cn(
-        `radio border-box relative flex h-6 w-6 cursor-default items-center
-justify-center rounded-full border-2 border-neutral-500 outline-none
-data-[state=checked]:border-none data-[state=checked]:bg-primary-600
-data-[state=checked]:after:block data-[state=checked]:after:h-3
-data-[state=checked]:after:w-3 data-[state=checked]:after:rounded-full
-data-[state=checked]:after:bg-white data-[state=checked]:after:content-['']
-dark:data-[state=checked]:bg-primary-200
-dark:data-[state=checked]:after:bg-primary-900`,
+        `radio border-box relative flex h-5 w-5 cursor-default items-center
+justify-center rounded-full border-2 border-neutral-600 outline-none
+aria-disabled:border-neutral-400 data-[state=checked]:border-none
+data-[state=checked]:bg-primary-500 data-[state=checked]:after:block
+data-[state=checked]:after:h-2.5 data-[state=checked]:after:w-2.5
+data-[state=checked]:after:rounded-full data-[state=checked]:after:bg-white
+data-[state=checked]:after:content-[''] disabled:border-neutral-400
+dark:border-neutral-400 dark:aria-disabled:border-neutral-600
+dark:data-[state=checked]:bg-primary-300 
+dark:data-[state=checked]:after:bg-primary-900 dark:disabled:border-neutral-600`,
         className
       )}
     />
