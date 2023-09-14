3af9bc074c6c460ab5baf684427b3e8d1f94fc5d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-14 21:31:41 +0300

    Update Radio styles to use ring instead of border

--- src/components/general/Radio/index.tsx
+++ src/components/general/Radio/index.tsx
@@ -16,16 +16,16 @@ export const Radio = forwardRef<HTMLButtonElement, RadioProps>(
       data-state={checked ? 'checked' : 'unchecked'}
       aria-disabled={disabled}
       className={cn(
-        `radio border-box relative flex h-5 w-5 cursor-default items-center
-justify-center rounded-full border-2 border-neutral-600 outline-none
-aria-disabled:border-neutral-400 data-[state=checked]:border-none
-data-[state=checked]:bg-primary-500 data-[state=checked]:after:block
+        `relative flex h-5 w-5 cursor-default items-center
+justify-center rounded-full outline-none ring-2 ring-inset ring-neutral-600
+aria-disabled:ring-neutral-400 data-[state=checked]:bg-primary-500
+data-[state=checked]:ring-0 data-[state=checked]:after:block
 data-[state=checked]:after:h-2.5 data-[state=checked]:after:w-2.5
 data-[state=checked]:after:rounded-full data-[state=checked]:after:bg-white
-data-[state=checked]:after:content-[''] disabled:border-neutral-400
-dark:border-neutral-400 dark:aria-disabled:border-neutral-600
+data-[state=checked]:after:content-[''] disabled:ring-neutral-400
+dark:ring-neutral-400 dark:aria-disabled:ring-neutral-600
 dark:data-[state=checked]:bg-primary-300 
-dark:data-[state=checked]:after:bg-primary-900 dark:disabled:border-neutral-600`,
+dark:data-[state=checked]:after:bg-primary-900 dark:disabled:ring-neutral-600`,
         className
       )}
     />
