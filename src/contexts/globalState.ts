b5657b978380a29055cd3f569aed0897f0555488
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-16 17:35:12 +0300

    Update global state danger types, comments & initial state

--- src/contexts/globalState.ts
+++ src/contexts/globalState.ts
@@ -1,18 +1,32 @@
 import { createContext } from 'react';
 import { randomHexColor } from 'utils';
-import type { NeutralColorSuggestion } from 'constants/colorSuggestions';
+import type {
+  DangerColorSuggestion,
+  NeutralColorSuggestion,
+} from 'constants/colorSuggestions';
 import type { AnyStringWithAutocomplete } from 'types';
 
 export interface GlobalState {
   baseColors: {
+    /** Any hex color. */
     primary: string;
+    /** Any of the neutral suggested color names, or any hex color.
+     * If undefined, auto choose based on the algorithm below. */
     neutral?: AnyStringWithAutocomplete<NeutralColorSuggestion>;
-    danger?: string;
+    /** Any of the neutral danger color names, or any hex color.
+     * If undefined, auto choose based on the algorithm below. */
+    danger?: AnyStringWithAutocomplete<DangerColorSuggestion>;
+    /** Extra colors, each can have a name and its value can be any of the
+     * general suggested color names, or any hex color. */
   };
 }
 
 export const initialState: GlobalState = {
-  baseColors: { primary: randomHexColor() },
+  baseColors: {
+    primary: randomHexColor(),
+    neutral: undefined,
+    danger: undefined,
+  },
 };
 const initialDispatch = () => {
   return;
