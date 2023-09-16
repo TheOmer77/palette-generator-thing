256442e201ed9861331dc8d75447a1d159007151
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-16 21:56:00 +0300

    Change Input focus ring behavior
    Change Input ring so it only changes to a focused state when the <input>
    itself is focused.

--- src/components/general/Input/index.tsx
+++ src/components/general/Input/index.tsx
@@ -28,21 +28,11 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <div
         className={cn(
           `relative flex h-12 items-center gap-2 rounded-lg bg-white px-2
-ring-1 ring-neutral-300 focus-within:ring-2 focus-within:ring-primary-500
-dark:bg-neutral-950 dark:ring-neutral-700 dark:focus-within:ring-primary-300
-[&>label]:focus-within:text-primary-500
-dark:[&>label]:focus-within:text-primary-300`,
+dark:bg-neutral-950`,
           label && 'mt-6',
           className
         )}
       >
-        <label
-          htmlFor={id}
-          className='absolute -top-6 select-none text-sm font-medium
-text-neutral-600 dark:text-neutral-400'
-        >
-          {label}
-        </label>
         {startAdornment}
         <InputComponent
           {...props}
@@ -50,8 +40,24 @@ text-neutral-600 dark:text-neutral-400'
           id={id}
           className='flex-grow bg-transparent text-neutral-900
 autofill-override-white focus-visible:outline-none dark:text-neutral-100
-dark:autofill-override-neutral-950'
+dark:autofill-override-neutral-950 [&+.input-outline>label]:focus:text-primary-500
+dark:[&+.input-outline>label]:focus:text-primary-300
+[&+.input-outline]:focus:ring-2
+[&+.input-outline]:focus:ring-primary-500
+dark:[&+.input-outline]:focus:ring-primary-300'
         />
+        <div
+          className='input-outline pointer-events-none absolute start-0 top-0
+h-full w-full rounded-lg ring-1 ring-neutral-300 dark:ring-neutral-700'
+        >
+          <label
+            htmlFor={id}
+            className='pointer-events-auto absolute -top-6 select-none text-sm
+font-medium text-neutral-600 dark:text-neutral-400'
+          >
+            {label}
+          </label>
+        </div>
         {endAdornment}
       </div>
     );
