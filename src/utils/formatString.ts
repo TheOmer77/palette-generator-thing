26db2aaa0193638ab662b0bc733bccb5b3fff515
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-16 17:54:01 +0300

    `toCamelCase` syntax updates

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -1,6 +1,4 @@
-export const toCamelCase = (str: string) => {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-    if (+match === 0) return ''; // or if (/\s+/.test(match)) for white spaces
-    return index === 0 ? match.toLowerCase() : match.toUpperCase();
-  });
-};
+export const toCamelCase = (str: string) =>
+  str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) =>
+    +match === 0 ? '' : index === 0 ? match.toLowerCase() : match.toUpperCase()
+  );
