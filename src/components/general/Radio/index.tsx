83dc754ce9204d474c74e4862f319523a695aa59
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-18 18:27:32 +0300

    Tweak Radio indicator & add transition on state change

--- src/components/general/Radio/index.tsx
+++ src/components/general/Radio/index.tsx
@@ -18,14 +18,14 @@ export const Radio = forwardRef<HTMLButtonElement, RadioProps>(
       className={cn(
         `relative flex h-5 w-5 cursor-default items-center
 justify-center rounded-full outline-none ring-2 ring-inset ring-neutral-600
-aria-disabled:ring-neutral-400 data-[state=checked]:bg-primary-500
-data-[state=checked]:ring-0 data-[state=checked]:after:block
-data-[state=checked]:after:h-2.5 data-[state=checked]:after:w-2.5
-data-[state=checked]:after:rounded-full data-[state=checked]:after:bg-white
-data-[state=checked]:after:content-[''] disabled:ring-neutral-400
-dark:ring-neutral-400 dark:aria-disabled:ring-neutral-600
-dark:data-[state=checked]:bg-primary-300 
-dark:data-[state=checked]:after:bg-primary-900 dark:disabled:ring-neutral-600`,
+transition-[background-color,box-shadow] after:h-2 after:w-2
+after:scale-0 after:rounded-full after:bg-white after:transition-transform
+after:content-[''] aria-disabled:ring-neutral-400
+data-[state=checked]:bg-primary-500 data-[state=checked]:ring-0
+data-[state=checked]:after:block data-[state=checked]:after:scale-100
+disabled:ring-neutral-400 dark:ring-neutral-400 dark:after:bg-primary-900
+dark:aria-disabled:ring-neutral-600 dark:data-[state=checked]:bg-primary-300
+dark:disabled:ring-neutral-600`,
         className
       )}
     />
