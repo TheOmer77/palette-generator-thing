2c2dd1982d988c91c98b6654707bd7d3a0f425d3
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-09-23 20:35:29 +0300

    Add Add and Delete icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,7 @@
+export { ReactComponent as AddIcon } from './add.svg';
 export { ReactComponent as CopyIcon } from './copy.svg';
+export { ReactComponent as DeleteIcon } from './delete.svg';
 export { ReactComponent as DoneIcon } from './done.svg';
+export { ReactComponent as ExpandMoreIcon } from './expandMore.svg';
 export { ReactComponent as RandomIcon } from './random.svg';
 export { ReactComponent as TuneIcon } from './tune.svg';
-export { ReactComponent as ExpandMoreIcon } from './expandMore.svg';
