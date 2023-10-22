5750ceab8ef9aa77b5d2e080d13b01acfbcfa2f3
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-22 22:21:24 +0300

    Increase z-index of ListSubheader

--- src/components/general/List/ListSubheader.tsx
+++ src/components/general/List/ListSubheader.tsx
@@ -10,7 +10,7 @@ export const ListSubheader = forwardRef<
       {...props}
       ref={ref}
       className={cn(
-        `sticky top-0 z-10 inline-block w-full select-none p-2 text-sm
+        `sticky top-0 z-20 inline-block w-full select-none p-2 text-sm
 font-medium text-neutral-600 dark:text-neutral-400`,
         className
       )}
