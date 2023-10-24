36824ab568b62aef182ed274bb60c89e01e159fa
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-24 13:06:17 +0300

    Increase z-index of Tooltips

--- src/components/general/Tooltip/index.tsx
+++ src/components/general/Tooltip/index.tsx
@@ -22,9 +22,9 @@ export const Tooltip = ({ title, side, className, children }: TooltipProps) => (
         <TooltipContent
           side={side}
           className={clsx(
-            `select-none rounded-lg bg-neutral-50 p-2 text-sm leading-none
-text-neutral-800 shadow-sm shadow-neutral-950/25 will-change-[transform,opacity]
-data-[state=closed]:animate-fadeout-fast
+            `z-30 select-none rounded-lg bg-neutral-50 p-2 text-sm leading-none
+text-neutral-800 shadow-sm shadow-neutral-950/25
+will-change-[transform,opacity] data-[state=closed]:animate-fadeout-fast
 data-[state=delayed-open]:data-[side=bottom]:animate-slideDownAndFade
 data-[state=delayed-open]:data-[side=left]:animate-slideLeftAndFade
 data-[state=delayed-open]:data-[side=right]:animate-slideRightAndFade
