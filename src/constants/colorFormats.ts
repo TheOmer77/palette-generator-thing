b752ef405cdfef4ea4281fe53a3f4ed1ceb03c0b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-24 19:50:31 +0300

    Add `toString` functions to LCH color formats

--- src/constants/colorFormats.ts
+++ src/constants/colorFormats.ts
@@ -1,4 +1,14 @@
-import { formatHex, formatHsl, formatRgb, type Color } from 'culori/fn';
+import {
+  formatCss,
+  formatHex,
+  formatHsl,
+  formatRgb,
+  modeLch,
+  useMode as loadMode,
+  type Color,
+} from 'culori/fn';
+
+const lch = loadMode(modeLch);
 
 type ColorFormat = {
   id: string;
@@ -34,8 +44,26 @@ export const colorFormats = [
     toString: color =>
       (formatHsl(color) as string).replaceAll(',', '').slice(4, -1),
   },
-  { id: 'lch', displayName: 'LCH' },
-  { id: 'lchRaw', displayName: 'LCH (Raw)' },
+  {
+    id: 'lch',
+    displayName: 'LCH',
+    toString: color =>
+      `lch(${(formatCss(lch(color)) as string)
+        .slice(4, -1)
+        .split(' ')
+        .map(num => Math.round(Number(num) * 100) / 100)
+        .join(' ')})`,
+  },
+  {
+    id: 'lchRaw',
+    displayName: 'LCH (Raw)',
+    toString: color =>
+      (formatCss(lch(color)) as string)
+        .slice(4, -1)
+        .split(' ')
+        .map(num => Math.round(Number(num) * 100) / 100)
+        .join(' '),
+  },
   { id: 'oklch', displayName: 'OKLCH' },
   { id: 'oklchRaw', displayName: 'OKLCH (Raw)' },
 ] as const satisfies readonly ColorFormat[];
