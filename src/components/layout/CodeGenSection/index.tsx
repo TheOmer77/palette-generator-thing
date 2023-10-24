2f07ccf4179c4ec83280ae9a7e921b898f9b83cf
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-10-24 14:12:01 +0300

    Add selectable color format options to CodeGenSection

--- src/components/layout/CodeGenSection/index.tsx
+++ src/components/layout/CodeGenSection/index.tsx
@@ -4,22 +4,27 @@ import {
   AccordionListItem,
   ListItemText,
   ListSubheader,
+  RadioGroup,
 } from 'components/general';
 import RadioListItem from '../BaseColorsSection/RadioListItem';
+import { colorFormats } from 'constants';
+import { useGlobalState } from 'hooks';
 
 const CodeGenSection = forwardRef<
   HTMLElement,
   ComponentPropsWithoutRef<'section'>
 >((props, ref) => {
-  // TODO: Actual functionality lol
+  const [{ codeGen }, setGlobalState] = useGlobalState();
+
   return (
     <section {...props} ref={ref}>
       <ListSubheader
         className='bg-white dark:bg-neutral-900 md:bg-neutral-50
-dark:md:bg-neutral-900'
+        dark:md:bg-neutral-900'
       >
         Generated code
       </ListSubheader>
+      {/* TODO: RadioGroup + actual functionality lol */}
       <AccordionListItem
         title={<ListItemText primary='Format' secondary='CSS variables' />}
         value='codeGen-format'
@@ -31,21 +36,29 @@ dark:md:bg-neutral-900'
         <RadioListItem disabled>Custom</RadioListItem>
       </AccordionListItem>
       {/* TODO: Show codeGen-colorFormat only if codeGen-format isn't None of Custom */}
-      <AccordionListItem
-        title={<ListItemText primary='Color format' secondary='RGB (raw)' />}
-        value='codeGen-colorFormat'
-        role='radiogroup'
+      <RadioGroup
+        asChild
+        value={codeGen.colorFormat}
+        onValueChange={newValue =>
+          setGlobalState({
+            codeGen: {
+              ...codeGen,
+              colorFormat: newValue as typeof codeGen.colorFormat,
+            },
+          })
+        }
       >
-        <RadioListItem disabled>Hex</RadioListItem>
-        <RadioListItem disabled>RGB</RadioListItem>
-        <RadioListItem checked>RGB (raw)</RadioListItem>
-        <RadioListItem disabled>HSL</RadioListItem>
-        <RadioListItem disabled>HSL (raw)</RadioListItem>
-        <RadioListItem disabled>LCH</RadioListItem>
-        <RadioListItem disabled>LCH (raw)</RadioListItem>
-        <RadioListItem disabled>OKLCH</RadioListItem>
-        <RadioListItem disabled>OKLCH (raw)</RadioListItem>
-      </AccordionListItem>
+        <AccordionListItem
+          title={<ListItemText primary='Color format' secondary='RGB (raw)' />}
+          value='codeGen-colorFormat'
+        >
+          {colorFormats.map(({ id, displayName }) => (
+            <RadioListItem key={id} value={id}>
+              {displayName}
+            </RadioListItem>
+          ))}
+        </AccordionListItem>
+      </RadioGroup>
     </section>
   );
 });
