e24c88a163d321bd44810e318fac6c73135e7589
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-13 14:59:35 +0200

    Add Shadcn UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss';
 import scrollbar from 'tailwind-scrollbar';
+import animate from 'tailwindcss-animate';
 import plugin from 'tailwindcss/plugin';
 
 //@ts-expect-error Javascript only, no type decleration
@@ -49,11 +50,18 @@ const customPlugin = plugin(({ addUtilities, matchUtilities, theme }) => {
   );
 });
 
-const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+const config = {
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: { '2xl': '1400px' },
+    },
     extend: {
       animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
         'collapse-in': 'collapse-in 300ms cubic-bezier(0.2, 1, 0.4, 1)',
         'collapse-out': 'collapse-out 300ms cubic-bezier(0.2, 1, 0.4, 1)',
         'fade-in': 'fade-in 300ms cubic-bezier(0.2, 1, 0.4, 1)',
@@ -63,12 +71,25 @@ const config: Config = {
         'tooltip-in': 'tooltip-in 150ms cubic-bezier(0.2, 1, 0.4, 1)',
         'tooltip-out': 'fade-out 150ms cubic-bezier(0.2, 1, 0.4, 1)',
       },
+      borderRadius: {
+        lg: 'var(--border-radius)',
+        md: 'calc(var(--border-radius) - 2px)',
+        sm: 'calc(var(--border-radius) - 4px)',
+      },
       fontFamily: {
         mono: ['var(--font-family-mono)', 'monospace'],
         sans: ['var(--font-family)', 'sans-serif'],
       },
       height: { screen: '100dvh' },
       keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
         'collapse-in': {
           from: { height: '0' },
           to: { height: 'var(--radix-collapsible-content-height)' },
@@ -149,6 +170,7 @@ const config: Config = {
       white: '#fff',
       black: '#000',
       transparent: 'transparent',
+
       ...['primary', 'neutral', 'danger'].reduce(
         (obj, colorName) => ({
           ...obj,
@@ -157,14 +179,39 @@ const config: Config = {
               ...obj,
               [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
             }),
-            {}
+            {
+              DEFAULT: `rgb(var(--color-${colorName}-main))`,
+              foreground: `rgb(var(--color-${colorName}-contrast))`,
+            }
           ),
         }),
         {}
       ),
+
+      border: 'rgb(var(--color-border))',
+      input: 'rgb(var(--color-input))',
+      ring: 'rgb(var(--color-ring))',
+      background: 'rgb(var(--color-background))',
+      foreground: 'rgb(var(--color-foreground))',
+      muted: {
+        DEFAULT: 'rgb(var(--color-muted))',
+        foreground: 'rgb(var(--color-muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'rgb(var(--color-accent))',
+        foreground: 'rgb(var(--color-accent-foreground))',
+      },
+      popover: {
+        DEFAULT: 'rgb(var(--color-popover))',
+        foreground: 'rgb(var(--color-popover-foreground))',
+      },
+      card: {
+        DEFAULT: 'rgb(var(--color-card))',
+        foreground: 'rgb(var(--color-card-foreground))',
+      },
     },
   },
-  plugins: [scrollbar, customPlugin],
-};
+  plugins: [scrollbar, animate, customPlugin],
+} satisfies Config;
 
 export default config;
