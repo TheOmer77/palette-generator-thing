69e36a70dda5e87f5947e3395a0b444a793e552f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-13 17:26:37 +0200

    Adjust ColorGrid columns

--- src/components/colors/ColorGrid.tsx
+++ src/components/colors/ColorGrid.tsx
@@ -10,8 +10,8 @@ export const ColorGrid = forwardRef<
     {...props}
     ref={ref}
     className={cn(
-      `mb-4 grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-2
-lg:grid-cols-4 xl:grid-cols-5`,
+      `mb-4 grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-3
+lg:grid-cols-5 xl:grid-cols-6`,
       className
     )}
   >
