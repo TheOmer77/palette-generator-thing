63af30abab00f0ff1e46034c317402400c038c25
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-25 22:37:21 +0200

    Add useBaseColors Zustand store

---
+++ src/store/useBaseColors.ts
@@ -0,0 +1,81 @@
+import { create } from 'zustand';
+
+import type {
+  DangerColorSuggestion,
+  GeneralColorSuggestion,
+  NeutralColorSuggestion,
+} from '@/types/defaultSuggestions';
+import type { AnyStringWithAutocomplete } from '@/types/utils';
+import { generalColorSuggestionNames } from '@/constants';
+
+export type BaseColorsState = {
+  /** Any hex color. */
+  primary: string;
+  /** Any of the neutral suggested color names, or any hex color.
+   * If undefined, auto choose. */
+  neutral?: AnyStringWithAutocomplete<NeutralColorSuggestion>;
+  /** Any of the neutral danger color names, or any hex color.
+   * If undefined, auto choose. */
+  danger?: AnyStringWithAutocomplete<DangerColorSuggestion>;
+  /** Extra colors, each can have a name and its value can be any of the
+   * general suggested color names, or any hex color. */
+  extras: {
+    name?: string;
+    value: AnyStringWithAutocomplete<GeneralColorSuggestion>;
+  }[];
+};
+
+export type BaseColorsSetters = {
+  setPrimary: (primary: BaseColorsState['primary']) => void;
+  setNeutral: (neutral: BaseColorsState['neutral']) => void;
+  setDanger: (danger: BaseColorsState['danger']) => void;
+  addExtraColor: () => void;
+  removeExtraColor: (index: number) => void;
+  renameExtraColor: (index: number, newName: string) => void;
+  setExtraColor: (
+    index: number,
+    newValue: AnyStringWithAutocomplete<GeneralColorSuggestion>
+  ) => void;
+};
+
+export type BaseColorsStore = BaseColorsState & BaseColorsSetters;
+
+// TODO: Store this state in URL
+export const useBaseColors = create<BaseColorsStore>(set => ({
+  // TEMPORARY HARDCODED VALUE
+  primary: '#1740ea',
+  neutral: undefined,
+  danger: undefined,
+  extras: [],
+
+  setPrimary: primary => set({ primary }),
+  setNeutral: neutral => set({ neutral }),
+  setDanger: danger => set({ danger }),
+  addExtraColor: () =>
+    set(state => ({
+      extras: [
+        ...state.extras,
+        {
+          name: '',
+          value:
+            generalColorSuggestionNames[
+              state.extras.length % generalColorSuggestionNames.length
+            ],
+        },
+      ],
+    })),
+  removeExtraColor: index =>
+    set(state => ({ extras: state.extras.filter?.((_, i) => i !== index) })),
+  renameExtraColor: (index, newName) =>
+    set(state => ({
+      extras: state.extras.map((color, i) =>
+        i === index ? { ...color, name: newName } : color
+      ),
+    })),
+  setExtraColor: (index, newValue) =>
+    set(state => ({
+      extras: state.extras.map((color, i) =>
+        i === index ? { ...color, value: newValue } : color
+      ),
+    })),
+}));
