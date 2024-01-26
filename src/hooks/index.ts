7fa9d924a01d9e3be1390fb1c82c46d88172ab8b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-26 14:07:19 +0200

    Completely remove previous global state

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-export { default as useGlobalState } from './useGlobalState';
+// TODO: Named exports
 export { default as useMediaQuery } from './useMediaQuery';
 export { default as useTailwindBreakpoint } from './useTailwindBreakpoint';
 export { default as useTheme } from './useTheme';
