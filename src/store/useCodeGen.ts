698f3af3ae4243add65dd32822155981a87c2e82
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-26 13:44:54 +0200

    Add useCodeGen Zustand store

---
+++ src/store/useCodeGen.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+import type { codeFormats, colorFormats } from '@/constants';
+
+export type CodeGenState = {
+  /** Format for generated theme code. */
+  format: keyof typeof codeFormats;
+  /** Color format for generated code.
+   * If format is `none` or `custom`, has no effect. */
+  colorFormat: keyof typeof colorFormats;
+};
+
+export type CodeGenSetters = {
+  setFormat: (format: CodeGenState['format']) => void;
+  setColorFormat: (colorFormat: CodeGenState['colorFormat']) => void;
+};
+
+export type CodeGenStore = CodeGenState & CodeGenSetters;
+
+export const useCodeGen = create<CodeGenStore>(set => ({
+  format: 'none',
+  colorFormat: 'hex',
+
+  setFormat: format => set({ format }),
+  setColorFormat: colorFormat => set({ colorFormat }),
+}));
