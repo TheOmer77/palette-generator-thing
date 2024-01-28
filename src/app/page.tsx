46ae8c0854e45ef2ec90bb92fcbb9b3c459f83a7
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-28 15:56:11 +0200

    Validate URL primary color
    Check if primary color in URL exists and is a valid hex color; If not,
    redirect to a URL with valid params.
    This will also allow custom validation for other params later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { parseHex } from 'culori/fn';
 
 import { Main, Options } from '@/components/layout';
 import { randomHexColor } from '@/lib/colorUtils';
 
 const HomePage = ({ searchParams }: { searchParams: { primary?: string } }) => {
-  const url = headers().get('x-url');
-  if (!searchParams.primary)
-    redirect(
-      `${url}?${new URLSearchParams({ primary: randomHexColor().slice(1), ...searchParams })}`
-    );
+  const url = headers().get('x-url')?.split('?')[0];
+
+  const validParams = {
+    primary:
+      typeof searchParams.primary === 'string' &&
+      parseHex(searchParams.primary || '')
+        ? searchParams.primary
+        : randomHexColor().slice(1),
+  };
+
+  Object.entries(validParams).forEach(([key, value]) => {
+    if (value !== searchParams[key as keyof typeof validParams])
+      redirect(`${url}?${new URLSearchParams(validParams)}`);
+  });
 
   return (
     <div className='mx-auto max-w-screen-2xl'>
