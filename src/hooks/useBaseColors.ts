fe7307d79c64e41a10e52380db828c2edbd4422f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-28 15:14:23 +0200

    Add neutral color to URL state

--- src/hooks/useBaseColors.ts
+++ src/hooks/useBaseColors.ts
@@ -1,17 +1,42 @@
+import { useCallback } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { parseHex } from 'culori/fn';
 
 import {
   useBaseColors as OLD_useBaseColors,
   type BaseColorsState,
 } from '@/store/useBaseColors';
-import { useCallback } from 'react';
+
+const colorToSearchParam = (hexColor?: string) =>
+  typeof hexColor === 'string'
+    ? hexColor.startsWith('#')
+      ? hexColor.slice(1)
+      : hexColor
+    : null;
+
+// TODO: Add param for allowed values other than hex colors
+const colorFromSearchParam = (paramColor: string | null) =>
+  typeof paramColor === 'string'
+    ? typeof parseHex(paramColor) !== 'undefined'
+      ? `#${paramColor}`
+      : paramColor
+    : undefined;
 
 export const useBaseColors = () => {
   const searchParams = useSearchParams();
 
   // TODO: Get rid of this, once I don't need it anymore
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { primary: _0, setPrimary: _1, ...rest } = OLD_useBaseColors();
+  const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    primary: _0,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setPrimary: _1,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    neutral: _2,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setNeutral: _3,
+    ...rest
+  } = OLD_useBaseColors();
 
   const setSearchParam = useCallback(
     (key: string, value: string | null) => {
@@ -24,16 +49,19 @@ export const useBaseColors = () => {
     [searchParams]
   );
 
-  const primary = `#${searchParams.get('primary')}`;
+  const primary = colorFromSearchParam(searchParams.get('primary')),
+    neutral = colorFromSearchParam(searchParams.get('neutral'));
 
   const setPrimary = useCallback(
-    (primary: BaseColorsState['primary']) =>
-      setSearchParam(
-        'primary',
-        primary.startsWith('#') ? primary.slice(1) : primary
-      ),
-    [setSearchParam]
-  );
+      (primary: BaseColorsState['primary']) =>
+        setSearchParam('primary', colorToSearchParam(primary)),
+      [setSearchParam]
+    ),
+    setNeutral = useCallback(
+      (neutral: BaseColorsState['neutral']) =>
+        setSearchParam('neutral', colorToSearchParam(neutral)),
+      [setSearchParam]
+    );
 
-  return { primary, setPrimary, ...rest };
+  return { primary, neutral, setPrimary, setNeutral, ...rest };
 };
