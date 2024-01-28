e2de802cb09c5f61c804a7ee77a7b425080f4673
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-01-28 13:52:52 +0200

    Make sure URL always has `primary` param
    Check if URL has a `primary` search param; If not, redirect to the same
    URL with the param set to a random color.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Main, Options } from '@/components/layout';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const HomePage = () => (
-  <div className='mx-auto max-w-screen-2xl'>
-    <Options />
-    <Main />
-  </div>
-);
+import { Main, Options } from '@/components/layout';
+import { randomHexColor } from '@/lib/colorUtils';
+
+const HomePage = ({ searchParams }: { searchParams: { primary?: string } }) => {
+  const url = headers().get('x-url');
+  if (!searchParams.primary)
+    redirect(
+      `${url}?${new URLSearchParams({ primary: randomHexColor().slice(1), ...searchParams })}`
+    );
+
+  return (
+    <div className='mx-auto max-w-screen-2xl'>
+      <Options />
+      <Main />
+    </div>
+  );
+};
 
 export default HomePage;
