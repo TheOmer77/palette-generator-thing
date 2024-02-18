d771096bb5ef84db23c8e9291b259b2859cf8e5f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-18 20:53:45 +0200

    Limit width of ColorPicker + format classname

--- src/components/ui/ColorPicker/ColorPicker.tsx
+++ src/components/ui/ColorPicker/ColorPicker.tsx
@@ -19,18 +19,16 @@ export const ColorPicker = ({
     className={cn(
       /* Using String.raw due to underscores in targeted classnames
       https://github.com/tailwindlabs/tailwindcss/issues/8881#issuecomment-1208600668 */
-      String.raw`[&.react-colorful]:h-auto [&.react-colorful]:w-auto
-[&_.react-colorful\_\_hue]:h-2
+      String.raw`mx-auto max-w-sm [&.react-colorful]:h-auto
+[&.react-colorful]:w-auto [&_.react-colorful\_\_hue]:h-2
 [&_.react-colorful\_\_hue]:rounded-full
 [&_.react-colorful\_\_interactive:focus_.react-colorful\_\_pointer]:scale-100
-[&_.react-colorful\_\_pointer]:h-5
-[&_.react-colorful\_\_pointer]:w-5
+[&_.react-colorful\_\_pointer]:h-5 [&_.react-colorful\_\_pointer]:w-5
 [&_.react-colorful\_\_pointer]:-translate-x-1/2
 [&_.react-colorful\_\_pointer]:-translate-y-1/2
 [&_.react-colorful\_\_pointer]:border-2
 [&_.react-colorful\_\_pointer]:border-white
-[&_.react-colorful\_\_pointer]:shadow-sm
-[&_.react-colorful\_\_saturation]:mb-4
+[&_.react-colorful\_\_pointer]:shadow-sm [&_.react-colorful\_\_saturation]:mb-4
 [&_.react-colorful\_\_saturation]:aspect-square
 [&_.react-colorful\_\_saturation]:w-full
 [&_.react-colorful\_\_saturation]:rounded-lg
