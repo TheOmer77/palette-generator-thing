758043ec2c25c9c3d96a299807fe8aefed96b7f7
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-02-19 19:06:19 +0200

    Rework ColorPicker with internal state
    This fixes a bug with the picker's hue slider. Yep, that's the only
    reason for this 🙃

--- src/components/ui/ColorPicker/ColorPicker.tsx
+++ src/components/ui/ColorPicker/ColorPicker.tsx
@@ -1,24 +1,36 @@
 import {
+  useCallback,
+  useEffect,
   useMemo,
+  useState,
   type CSSProperties,
   type ComponentPropsWithoutRef,
 } from 'react';
-import { HexColorPicker } from 'react-colorful';
-import { formatHex, modeHsl, useMode as loadMode } from 'culori/fn';
-
-import { cn } from '@/lib/utils';
-import { Slider } from '@/components/ui/Slider';
+import { HslColorPicker, type HslColor } from 'react-colorful';
+import { formatHex, modeHsl, useMode as loadMode, type Color } from 'culori/fn';
 import type { SliderProps } from '@radix-ui/react-slider';
 
+import { Slider } from '@/components/ui/Slider';
+import { cn } from '@/lib/utils';
+
 export type ColorPickerProps = Omit<
-  ComponentPropsWithoutRef<typeof HexColorPicker>,
-  'color'
+  ComponentPropsWithoutRef<typeof HslColorPicker>,
+  'color' | 'onChange'
 > & {
-  value?: ComponentPropsWithoutRef<typeof HexColorPicker>['color'];
+  value?: string;
+  onChange?: (newValue: string) => void;
   orientation?: SliderProps['orientation'];
 };
 
 const hsl = loadMode(modeHsl);
+const pickerHsl = (color?: string | Color) => {
+  const hslValue = hsl(color);
+  return {
+    h: hslValue?.h || 0,
+    s: (hslValue?.s || 0) * 100,
+    l: (hslValue?.l || 0) * 100,
+  } satisfies HslColor;
+};
 
 export const ColorPicker = ({
   value,
@@ -27,13 +39,55 @@ export const ColorPicker = ({
   className,
   ...props
 }: ColorPickerProps) => {
-  const valueHsl = useMemo(() => hsl(value), [value]);
+  const [internalValue, setInternalValue] = useState<HslColor>(
+    pickerHsl(value)
+  );
+
+  const sliderVars = useMemo(
+    () =>
+      ({
+        '--thumb-color': `hsl(${internalValue.h} 100% 50%)`,
+        '--gradient-angle': orientation === 'horizontal' ? '180deg' : '90deg',
+      }) as CSSProperties,
+    [internalValue.h, orientation]
+  );
+
+  const handleValueChange = useCallback(
+    (hslColor: Partial<HslColor>) => {
+      const prevValueHsl = { ...internalValue },
+        newValueHsl = { ...prevValueHsl, ...hslColor };
+
+      setInternalValue(prev => ({ ...prev, ...hslColor }));
+
+      const valueHex = formatHex(value),
+        newValueHex = formatHex({
+          mode: 'hsl',
+          h: newValueHsl.h,
+          s: newValueHsl.s / 100,
+          l: newValueHsl.l / 100,
+        });
+      if (valueHex !== newValueHex) onChange?.(newValueHex);
+    },
+    [internalValue, onChange, value]
+  );
+
+  useEffect(() => {
+    // Reformat as hex to handle 3 digit hex colors
+    const valueHex = formatHex(value),
+      internalValueHex = formatHex({
+        mode: 'hsl',
+        h: internalValue.h,
+        s: internalValue.s / 100,
+        l: internalValue.l / 100,
+      });
+    if (internalValueHex !== valueHex) setInternalValue(pickerHsl(value));
+  }, [internalValue, value]);
 
   return (
     <div
       className={cn(
         /* Using String.raw due to underscores in targeted classnames
-      https://github.com/tailwindlabs/tailwindcss/issues/8881#issuecomment-1208600668 */
+        https://github.com/tailwindlabs/tailwindcss/issues/8881#issuecomment-1208600668 */
         String.raw`mx-auto flex h-full w-full max-w-sm gap-4
 [&_.react-colorful>:not(.react-colorful\_\_saturation)]:hidden
 [&_.react-colorful\_\_interactive:focus_.react-colorful\_\_pointer]:scale-100
@@ -52,33 +106,20 @@ export const ColorPicker = ({
         className
       )}
     >
-      <HexColorPicker {...props} color={value} onChange={onChange} />
+      <HslColorPicker
+        {...props}
+        color={internalValue}
+        onChange={handleValueChange}
+      />
       <Slider
         data-hue-slider=''
         orientation={orientation === 'horizontal' ? 'vertical' : 'horizontal'}
         inverted={orientation === 'horizontal'}
         min={0}
         max={360}
-        value={[valueHsl?.h || 0]}
-        onValueChange={([value]) =>
-          // BUG: value = 360 actually sets hue to 0
-          // Need to use internal HSL state instead of directly parsing value prop
-          onChange?.(
-            formatHex({
-              mode: 'hsl',
-              h: value,
-              s: valueHsl?.s || 0,
-              l: valueHsl?.l || 0,
-            })
-          )
-        }
-        style={
-          {
-            '--thumb-color': `hsl(${valueHsl?.h || 0} 100% 50%)`,
-            '--gradient-angle':
-              orientation === 'horizontal' ? '180deg' : '90deg',
-          } as CSSProperties
-        }
+        value={[internalValue.h]}
+        onValueChange={([value]) => handleValueChange({ h: value })}
+        style={sliderVars}
         className='data-[orientation=vertical]:h-auto
 [&_[data-slider-range]]:bg-transparent [&_[data-slider-thumb]]:border-white
 [&_[data-slider-thumb]]:bg-[--thumb-color] [&_[data-slider-thumb]]:shadow-sm
