7afc01d0f4cfea3ff69855e517f8f81a2e8919d1
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-03 17:10:23 +0200

    Redesign ExtraColorEditPage to use tabs
    Use tabs for suggestions and custom, instead of a radio group.
    Name input and remove list item still look the same.

--- src/components/layout/OptionsDrawer/ExtraColorEditPage.tsx
+++ src/components/layout/OptionsDrawer/ExtraColorEditPage.tsx
@@ -8,8 +8,6 @@ import { TrashIcon } from 'lucide-react';
 
 import { ColorEditPage } from './ColorEditPage';
 import { DebouncedColorPicker } from './DebouncedColorPicker';
-import RadioListItem from '../RadioListItem';
-import { Collapsible } from '@/components/ui/Collapsible';
 import { Input } from '@/components/ui/Input';
 import {
   List,
@@ -17,8 +15,7 @@ import {
   ListItemIcon,
   ListSubheader,
 } from '@/components/ui/List';
-import { RadioGroup } from '@/components/ui/Radio';
-import { Separator } from '@/components/ui/Separator';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs';
 import { ColorSuggestion, ColorSuggestionsBox } from '@/components/colors';
 import { useBaseColors } from '@/hooks/useBaseColors';
 import { useOptionsDrawer } from '@/store/useOptionsDrawer';
@@ -51,24 +48,16 @@ export const ExtraColorEditPage = forwardRef<
   const colorIsSuggestion = generalColorSuggestionNames.includes(value),
     colorIsCustom = !generalColorSuggestionNames.includes(value);
 
-  const radioValue = colorIsSuggestion ? 'suggestions' : 'custom';
   const themeValue = colorIsSuggestion
     ? generalColorSuggestions[value as GeneralColorSuggestion]?.(primary)
     : value;
 
   const handleValueChange = useCallback(
-    (newValue?: string) =>
-      ((newValue === 'suggestions' && !colorIsSuggestion) ||
-        (newValue === 'custom' && !colorIsCustom)) &&
-      setExtraColor(
-        index,
-        newValue === 'suggestions'
-          ? generalColorSuggestionNames[
-              index % generalColorSuggestionNames.length
-            ]
-          : generalColorSuggestions[value as GeneralColorSuggestion](primary)
-      ),
-    [colorIsCustom, colorIsSuggestion, index, primary, setExtraColor, value]
+    (newValue: string) => {
+      if (value === newValue) return;
+      setExtraColor(index, newValue);
+    },
+    [index, setExtraColor, value]
   );
 
   const handleRemove = useCallback(() => {
@@ -84,41 +73,30 @@ export const ExtraColorEditPage = forwardRef<
       color={themeValue}
       className={cn(
         `[&_[data-radix-scroll-area-viewport]]:pb-[4.5rem]
-[&_[data-scroll-bar]]:z-10`,
+[&_[data-radix-scroll-area-viewport]]:pt-[2px] [&_[data-scroll-bar]]:z-10`,
         className
       )}
     >
-      <List
-        className='px-0 [&>li:first-of-type]:mb-2 [&>li:first-of-type]:mt-[2px]
-[&>li:last-of-type]:absolute [&>li:last-of-type]:inset-x-0
-[&>li:last-of-type]:bottom-0 [&>li:last-of-type]:z-10
-[&>li:last-of-type]:flex [&>li:last-of-type]:h-14
-[&>li:last-of-type]:flex-row [&>li:last-of-type]:items-center
-[&>li:last-of-type]:bg-background [&>li:last-of-type]:px-4
-md:[&>li:last-of-type]:rounded-es-lg dark:[&>li:last-of-type]:bg-card'
+      <Input
+        label='Name'
+        value={name || ''}
+        onChange={e => renameExtraColor(index, e.target.value)}
+      />
+      <Tabs
+        defaultValue={colorIsCustom ? 'custom' : 'suggestions'}
+        className='mt-2'
       >
-        <ListItem asChild>
-          <Input
-            label='Name'
-            value={name || ''}
-            onChange={e => renameExtraColor(index, e.target.value)}
-          />
-        </ListItem>
-        <RadioGroup value={radioValue} onValueChange={handleValueChange}>
-          <RadioListItem value='suggestions'>Suggestions</RadioListItem>
-          <RadioListItem value='custom'>Custom</RadioListItem>
-        </RadioGroup>
-        <Separator />
-        <Collapsible open={colorIsSuggestion}>
+        <TabsList className='grid w-full grid-cols-2'>
+          <TabsTrigger value='suggestions'>Suggestions</TabsTrigger>
+          <TabsTrigger value='custom'>Custom</TabsTrigger>
+        </TabsList>
+        <TabsContent value='suggestions'>
           <ColorSuggestionsBox
-            value={value as GeneralColorSuggestion}
-            onValueChange={suggestionName =>
-              setExtraColor(index, suggestionName)
-            }
+            value={value || 'auto'}
+            onValueChange={handleValueChange}
+            className='p-0 [&>span]:col-span-full [&>span]:px-1'
           >
-            <ListSubheader className='col-span-full px-0'>
-              Suggestions
-            </ListSubheader>
+            <ListSubheader>Extra color suggestions</ListSubheader>
             {Object.entries(generalColorSuggestions).map(
               ([value, variantFn]) => (
                 <ColorSuggestion
@@ -129,15 +107,15 @@ md:[&>li:last-of-type]:rounded-es-lg dark:[&>li:last-of-type]:bg-card'
               )
             )}
           </ColorSuggestionsBox>
-        </Collapsible>
-        <Collapsible open={colorIsCustom}>
-          <div className='mt-2'>
-            <DebouncedColorPicker
-              initialValue={value || ''}
-              onChange={newColor => setExtraColor(index, newColor)}
-            />
-          </div>
-        </Collapsible>
+        </TabsContent>
+        <TabsContent value='custom'>
+          <DebouncedColorPicker
+            initialValue={themeValue}
+            onChange={handleValueChange}
+          />
+        </TabsContent>
+      </Tabs>
+      <List className='absolute inset-x-0 bottom-0 z-10 h-14 justify-center'>
         <ListItem onClick={handleRemove}>
           <ListItemIcon>
             <TrashIcon />
