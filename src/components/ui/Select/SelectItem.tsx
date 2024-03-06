f55c303c257961b670db80a4f206a3a845856654
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-03-06 22:04:28 +0200

    Tweak SelectItem styles

--- src/components/ui/Select/SelectItem.tsx
+++ src/components/ui/Select/SelectItem.tsx
@@ -19,9 +19,11 @@ export const SelectItem = forwardRef<
   <Item
     ref={ref}
     className={cn(
-      `relative flex w-full cursor-default select-none items-center rounded-sm
-py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none
-data-[disabled]:opacity-50 focus:bg-accent focus:text-accent-foreground`,
+      `relative flex h-9 w-full cursor-default select-none items-center
+rounded-sm pl-8 pr-2 text-sm outline-none transition-[background-color]
+duration-100 state-layer data-[disabled]:pointer-events-none
+data-[disabled]:opacity-50 focus:state-layer-muted/30 active:bg-muted/30
+active:duration-0 [&>*]:z-10`,
       className
     )}
     {...props}
