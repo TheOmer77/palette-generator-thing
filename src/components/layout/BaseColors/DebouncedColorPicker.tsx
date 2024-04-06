bf5f826f95b9aad208b35feab249ad359f435af3
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-06 13:33:34 +0300

    Fix incorrect debouncing in DebouncedColorPicker
    Fix bug (in usehooks-ts?) where DebouncedColorPicker's value isn't
    debounced, just delayed.

--- src/components/layout/BaseColors/DebouncedColorPicker.tsx
+++ src/components/layout/BaseColors/DebouncedColorPicker.tsx
@@ -1,5 +1,10 @@
-import { forwardRef, useCallback, useState, type ElementRef } from 'react';
-import { useDebounceCallback } from 'usehooks-ts';
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useState,
+  type ElementRef,
+} from 'react';
 
 import { ColorInput, type ColorInputProps } from '@/components/ui/ColorInput';
 import { ColorPicker } from '@/components/ui/ColorPicker';
@@ -13,28 +18,33 @@ export const DebouncedColorPicker = forwardRef<
   ElementRef<typeof ColorInput>,
   DebouncedColorPickerProps
 >(({ initialValue, onChange, className, ...props }, ref) => {
-  const [pickerValue, setPickerValue] = useState(initialValue);
-
-  const debounceValue = useDebounceCallback(
-    (value: string) => onChange?.(value),
-    200
-  );
+  const [pickerValue, setPickerValue] = useState(initialValue),
+    [debouncedValue, setDebouncedValue] = useState(initialValue);
 
   const handlePickerChange = useCallback(
     (newValue: string) => {
       setPickerValue(newValue);
-      if (pickerValue !== newValue) debounceValue(newValue);
+      if (pickerValue !== newValue) setDebouncedValue(newValue);
     },
-    [debounceValue, pickerValue]
+    [setDebouncedValue, pickerValue]
   );
 
   const handleInputChange = useCallback(
     (newValue: string) => {
       onChange?.(newValue);
+      setDebouncedValue(newValue);
       setPickerValue(newValue);
     },
     [onChange]
   );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (debouncedValue && debouncedValue !== initialValue)
+        onChange?.(debouncedValue);
+    }, 200);
+    return () => clearTimeout(timeout);
+  }, [debouncedValue, initialValue, onChange]);
 
   return (
     <>
