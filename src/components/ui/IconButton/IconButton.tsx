97665068db8bc4f2078e4a797f40255e9174df38
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-18 16:00:59 +0300

    Add `asChild` prop to IconButton

--- src/components/ui/IconButton/IconButton.tsx
+++ src/components/ui/IconButton/IconButton.tsx
@@ -1,24 +1,28 @@
 import { Children, forwardRef, type ComponentPropsWithoutRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 import { cn } from '@/lib/utils';
 
 export const IconButton = forwardRef<
   HTMLButtonElement,
-  ComponentPropsWithoutRef<'button'>
->(({ children, className, ...props }, ref) => (
-  <button
-    {...props}
-    ref={ref}
-    className={cn(
-      `flex size-12 cursor-default items-center justify-center rounded-lg
+  ComponentPropsWithoutRef<'button'> & { asChild?: boolean }
+>(({ asChild, children, className, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp
+      {...props}
+      ref={ref}
+      className={cn(
+        `flex size-12 cursor-default items-center justify-center rounded-lg
 text-lg ring-0 ring-ring transition-[color,background-color,box-shadow]
 duration-100 state-layer hover:state-layer-muted/30 focus-visible:outline-none
 focus-visible:ring-2 focus-visible:state-layer-muted/30 active:bg-muted/30
 active:duration-0 md:size-10 md:text-base [&>*]:z-10`,
-      className
-    )}
-  >
-    {children && Children.only(children)}
-  </button>
-));
+        className
+      )}
+    >
+      {children && Children.only(children)}
+    </Comp>
+  );
+});
 IconButton.displayName = 'IconButton';
