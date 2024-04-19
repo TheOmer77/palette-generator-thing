3093fd24e301964279fbc16a791d24c3a6f48bd3
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-19 18:34:48 +0300

    Fix navbar width & position on larger screens

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -3,9 +3,10 @@ import { NavbarHeader } from './NavbarHeader';
 
 export const Navbar = () => (
   <nav
-    className='w- sticky top-0 z-10 grid h-16 w-full grid-cols-[auto,1fr]
-items-center bg-background md:grid-cols-[theme(spacing.80),1fr]
-md:bg-transparent [&>:first-child]:ps-4 [&>:last-child]:pe-4'
+    className='sticky top-0 z-10 mx-auto grid h-16 w-full max-w-screen-2xl
+grid-cols-[auto,1fr] items-center bg-background
+md:grid-cols-[theme(spacing.80),1fr] md:bg-transparent [&>:first-child]:ps-4
+[&>:last-child]:pe-4'
   >
     <NavbarHeader />
     <NavbarActions />
