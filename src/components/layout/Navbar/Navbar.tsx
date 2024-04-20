d4428f171f2da1ea1e2a2c0cbe6517a33b9e90b0
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-20 19:41:48 +0300

    Hide navbar on print

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
           `sticky top-0 z-10 mx-auto grid h-16 w-full max-w-screen-2xl
 grid-cols-[auto,1fr] items-center bg-background transition-[background-color]
 md:grid-cols-[theme(spacing.80),1fr] md:bg-transparent 
-[&>:first-child]:ps-4 [&>:last-child]:pe-4`,
+print:hidden [&>:first-child]:ps-4 [&>:last-child]:pe-4`,
           pathnameIsRoot &&
             `before:absolute before:-z-10 before:size-full
 before:bg-gradient-to-b before:from-background before:via-background/90
