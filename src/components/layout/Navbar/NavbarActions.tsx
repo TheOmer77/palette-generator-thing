853971f6813cf5c34e16a6f8fd7a048e6c8f736e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-27 16:16:53 +0300

    Add NavbarThemeMenu

--- src/components/layout/Navbar/NavbarActions.tsx
+++ src/components/layout/Navbar/NavbarActions.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { NavbarThemeMenu } from './NavbarThemeMenu';
 import { GithubLogo } from '../GithubLogo';
 import { IconButton } from '@/components/ui/IconButton';
 import { Tooltip } from '@/components/ui/Tooltip';
@@ -7,6 +8,7 @@ import { REPO_URL } from '@/constants/navbar';
 
 export const NavbarActions = () => (
   <div className='flex flex-row gap-px'>
+    <NavbarThemeMenu />
     <Tooltip content='GitHub repo'>
       <IconButton asChild>
         <Link href={REPO_URL}>
