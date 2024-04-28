8ebd146a299833d308884ca0e1f7f4e5eac53b4f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-04-28 20:57:15 +0300

    Update IconButton to be based on Button
    Update IconButton component to be based on Button, and use its styles,
    variants and sizes

--- src/components/ui/IconButton/IconButton.tsx
+++ src/components/ui/IconButton/IconButton.tsx
@@ -1,28 +1,13 @@
-import { Children, forwardRef, type ComponentPropsWithoutRef } from 'react';
-import { Slot } from '@radix-ui/react-slot';
+import { Children, forwardRef, type ElementRef } from 'react';
 
+import { Button, type ButtonProps } from '../Button';
 import { cn } from '@/lib/utils';
 
-export const IconButton = forwardRef<
-  HTMLButtonElement,
-  ComponentPropsWithoutRef<'button'> & { asChild?: boolean }
->(({ asChild, children, className, ...props }, ref) => {
-  const Comp = asChild ? Slot : 'button';
-  return (
-    <Comp
-      {...props}
-      ref={ref}
-      className={cn(
-        `flex size-12 cursor-default items-center justify-center rounded-lg
-text-lg ring-0 ring-ring transition-[color,background-color,box-shadow]
-duration-100 state-layer hover:state-layer-muted/30 focus-visible:outline-none
-focus-visible:ring-2 focus-visible:state-layer-muted/30 active:bg-muted/30
-active:duration-0 md:size-10 md:text-base [&>*]:z-10`,
-        className
-      )}
-    >
+export const IconButton = forwardRef<ElementRef<typeof Button>, ButtonProps>(
+  ({ children, className, ...props }, ref) => (
+    <Button {...props} ref={ref} className={cn('aspect-square p-0', className)}>
       {children && Children.only(children)}
-    </Comp>
-  );
-});
+    </Button>
+  )
+);
 IconButton.displayName = 'IconButton';
