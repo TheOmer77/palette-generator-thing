425e80159434438721ca588c0715b177f28143d0
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-04 15:33:14 +0300

    Update next config for PWA
    Add PWA config, and remove `reactStrictMode` as it's true by default.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
-const config = { reactStrictMode: true };
+//@ts-check
 
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({ dest: 'public' });
+const config = withPWA();
 export default config;
