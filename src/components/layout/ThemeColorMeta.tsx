fc584a8ee38f2943a25ce75d7c91c2ce70f686a8
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-08 22:19:57 +0300

    Darken theme-color meta when drawer is open
    In light theme, darken the theme-color meta value to match the drawer
    overlay color and opacity, overlaid on top of the background. No need to
    do this in dark theme as BG color and overlay color are the same.

--- src/components/layout/ThemeColorMeta.tsx
+++ src/components/layout/ThemeColorMeta.tsx
@@ -1,23 +1,69 @@
 'use client';
 
-import { Suspense, useMemo } from 'react';
+import { Suspense, useEffect, useMemo, useState } from 'react';
 
 import { useComputedBaseColors } from '@/hooks/useComputedBaseColors';
 import { useReadonlyTheme } from '@/store/useReadonlyTheme';
-import { getPaletteColor } from '@/lib/colorUtils';
+import { getPaletteColor, overlayColors } from '@/lib/colorUtils';
+
+const lightBgColor = '#ffffff',
+  fallbackColor = '#000000';
+
+const nodeIsVaulOverlay = (node: Node): node is HTMLDivElement =>
+  node instanceof HTMLDivElement &&
+  node.attributes.getNamedItem('vaul-overlay') !== null;
 
 const ThemeColorMetaContent = () => {
   const { neutral } = useComputedBaseColors();
   const { resolvedTheme } = useReadonlyTheme();
 
-  const themeColorMetaValue = useMemo(() => {
-    if (!resolvedTheme) return;
-    return resolvedTheme === 'light'
-      ? '#ffffff'
-      : getPaletteColor(neutral, 950);
-  }, [neutral, resolvedTheme]);
+  const [overlayOpacity, setOverlayOpacity] = useState(0);
 
-  return <meta name='theme-color' content={themeColorMetaValue || '#000000'} />;
+  const darkBgColor = useMemo(() => getPaletteColor(neutral, 950), [neutral]);
+  const themeColorMetaValue = useMemo(() => {
+    if (!resolvedTheme) return fallbackColor;
+
+    if (resolvedTheme === 'dark') return darkBgColor;
+    if (overlayOpacity <= 0) return lightBgColor;
+    return overlayColors(lightBgColor, darkBgColor, overlayOpacity * 0.5);
+  }, [darkBgColor, overlayOpacity, resolvedTheme]);
+
+  useEffect(() => {
+    const overlayStyleObserver = new MutationObserver(([mutation]) => {
+      if (nodeIsVaulOverlay(mutation.target)) {
+        const opacity = Number(mutation.target.style.opacity);
+        setOverlayOpacity(opacity);
+      }
+    });
+
+    const overlayMountObserver = new MutationObserver(mutations => {
+      const addedOverlay = mutations
+          .reduce((arr, curr) => [...arr, ...curr.addedNodes], [] as Node[])
+          .find(nodeIsVaulOverlay),
+        removedOverlay = mutations
+          .reduce((arr, curr) => [...arr, ...curr.removedNodes], [] as Node[])
+          .find(nodeIsVaulOverlay);
+
+      if (addedOverlay) {
+        overlayStyleObserver.observe(addedOverlay, {
+          attributeFilter: ['style'],
+        });
+        setOverlayOpacity(1);
+      }
+      if (removedOverlay) {
+        overlayStyleObserver.disconnect();
+        setOverlayOpacity(0);
+      }
+    });
+
+    overlayMountObserver.observe(document.body, { childList: true });
+    return () => {
+      overlayMountObserver.disconnect();
+      overlayStyleObserver.disconnect();
+    };
+  }, []);
+
+  return <meta name='theme-color' content={themeColorMetaValue} />;
 };
 
 export const ThemeColorMeta = () => (
