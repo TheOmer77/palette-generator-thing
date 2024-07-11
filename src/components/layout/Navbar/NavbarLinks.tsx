0f052bacba1830644dbee13db8001c7de7744f53
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-11 15:12:48 +0300

    Add safe area bottom padding to bottom nav
    This should mean the bottom gesture bar on iOS wouldn't cover the bottom
    nav links.

--- src/components/layout/Navbar/NavbarLinks.tsx
+++ src/components/layout/Navbar/NavbarLinks.tsx
@@ -16,10 +16,7 @@ const NavbarLinksContent = () => {
     <ToggleGroup
       type='single'
       value={pathname}
-      className='fixed inset-x-0 bottom-0 z-20 flex h-16 w-full flex-row
-items-center gap-px bg-card p-1 text-card-foreground shadow-md md:static
-md:inset-x-auto md:size-auto md:bg-transparent md:shadow-none [&>*]:size-full
-md:[&>*]:h-10 md:[&>*]:w-auto'
+      className='fixed inset-x-0 bottom-0 z-20 flex h-[calc(theme(spacing.16)+env(safe-area-inset-bottom))] w-full flex-row items-center gap-px bg-card p-1 pb-[calc(theme(spacing.1)+env(safe-area-inset-bottom))] text-card-foreground shadow-md md:static md:inset-x-auto md:size-auto md:bg-transparent md:pb-1 md:shadow-none [&>*]:size-full md:[&>*]:h-10 md:[&>*]:w-auto'
     >
       {NAVBAR_LINKS.map(({ href, icon, label }) => {
         const Comp = pathname === href ? 'div' : LinkWithSearchParams;
@@ -27,8 +24,7 @@ md:[&>*]:h-10 md:[&>*]:w-auto'
           <ToggleGroupItem key={href} value={href} asChild>
             <Button
               variant='flat'
-              className='flex-col gap-1 aria-checked:bg-muted/15 md:flex-row
-md:gap-2'
+              className='flex-col gap-1 aria-checked:bg-muted/15 md:flex-row md:gap-2'
               asChild
             >
               <Comp href={href}>
@@ -41,8 +37,7 @@ md:gap-2'
       })}
       <Separator
         orientation='vertical'
-        className='hidden data-[orientation=vertical]:mx-4
-data-[orientation=vertical]:h-6 md:block'
+        className='hidden data-[orientation=vertical]:mx-4 data-[orientation=vertical]:h-6 md:block'
       />
     </ToggleGroup>
   );
