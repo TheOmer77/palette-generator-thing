98462b32f63af30f97739bb1871bbcca183d87d4
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-15 17:56:53 +0300

    Add useModal hook

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import {
+  MODAL_BASECOLORS_EDIT,
+  MODAL_BASECOLORS_LIST,
+  MODAL_SEARCH_KEY,
+} from '@/constants/modalSearchParams';
+
+type ModalValue =
+  | null
+  | typeof MODAL_BASECOLORS_LIST
+  | `${typeof MODAL_BASECOLORS_EDIT}${string}`;
+
+export const useModal = () => {
+  const searchParams = useSearchParams(),
+    currentModal = searchParams.get(MODAL_SEARCH_KEY) as ModalValue;
+
+  const [lastModal, setLastModal] = useState(currentModal);
+
+  useEffect(() => {
+    if (currentModal !== null) setLastModal(currentModal);
+  }, [currentModal]);
+
+  const openModal = (modal: ModalValue) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (modal === null) params.delete(MODAL_SEARCH_KEY);
+    else params.set(MODAL_SEARCH_KEY, modal);
+    return window.history.pushState(null, '', `?${params.toString()}`);
+  };
+
+  const closeModal = (delta = -1) => window.history.go(delta);
+
+  return { currentModal, lastModal, openModal, closeModal };
+};
