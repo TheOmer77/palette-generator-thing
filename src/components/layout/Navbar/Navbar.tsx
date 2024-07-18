de2fd6e1d3d7af7b5151aa14e81ac4a2e0ab2619
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-18 13:30:26 +0300

    Fix navbar not centered correctly

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -19,14 +19,15 @@ export const Navbar = () => {
     <>
       <nav
         className={cn(
-          `fixed top-0 z-20 mx-auto grid h-16 w-full max-w-screen-2xl
+          `fixed top-0 z-20 grid h-16 w-full max-w-screen-2xl
 grid-cols-[auto,1fr] items-center bg-background transition-[background-color]
 before:absolute before:top-0 before:-z-10 before:size-full
 before:bg-gradient-to-b before:from-background before:via-background/90
-before:via-35% before:to-background/0 before:opacity-0
-before:transition-opacity md:grid-cols-[theme(spacing.80),1fr] md:bg-transparent md:before:end-0
-md:before:h-24 md:before:w-[calc(100%-theme(spacing.80))] md:before:opacity-100
-print:hidden [&>:first-child]:ps-4 [&>:last-child]:pe-4`,
+before:via-35% before:to-background/0 before:opacity-0 before:transition-opacity
+md:left-1/2 md:-translate-x-1/2 md:grid-cols-[theme(spacing.80),1fr]
+md:bg-transparent md:before:end-0 md:before:h-24
+md:before:w-[calc(100%-theme(spacing.80))] md:before:opacity-100 print:hidden
+[&>:first-child]:ps-4 [&>:last-child]:pe-4`,
           pathnameIsRoot && hideNavHeader && 'bg-transparent before:opacity-100'
         )}
       >
@@ -38,7 +39,7 @@ print:hidden [&>:first-child]:ps-4 [&>:last-child]:pe-4`,
           <NavbarActions />
         </div>
       </nav>
-      <div className="w-full h-16"/>
+      <div className='h-16 w-full' />
       <NavbarLargeHeader onIntersectingChange={setHideNavHeader} />
     </>
   );
