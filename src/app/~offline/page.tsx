22036c66167fffc08b06004c9a3b629bc98086c5
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-22 13:54:23 +0300

    Add offline fallback page (content is temporary)

---
+++ src/app/~offline/page.tsx
@@ -0,0 +1,14 @@
+const OfflineFallbackPage = () => {
+  return (
+    <div className='absolute start-0 top-0 flex min-h-dvh w-full select-none flex-col items-center justify-center p-4'>
+      <h1 className='m-0 text-9xl font-extrabold leading-none tracking-tight text-danger-800 dark:text-danger-200'>
+        Offline
+      </h1>
+      <p className='m-0 text-center text-xl text-neutral-700 dark:text-neutral-300'>
+        This is a temporary offline fallback page. Actual fallback page TBD.
+      </p>
+    </div>
+  );
+};
+
+export default OfflineFallbackPage;
