ea7efba77aa0a9cf23e2c7e2ba3d91b81cb8274c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-23 18:49:43 +0300

    Precache `/~offline`

--- src/sw.ts
+++ src/sw.ts
@@ -1,10 +1,11 @@
-import { Serwist, type PrecacheEntry, type SerwistGlobalConfig } from 'serwist';
+import {
+  Serwist,
+  type PrecacheEntry,
+  type PrecacheFallbackEntry,
+  type SerwistGlobalConfig,
+} from 'serwist';
 import { defaultCache } from '@serwist/next/worker';
 
-/* This declares the value of `injectionPoint` to TypeScript.
-`injectionPoint` is the string that will be replaced by the
-actual precache manifest. By default, this string is set to
-`"self.__SW_MANIFEST"`. */
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
@@ -13,12 +14,22 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
+const fallbackEntries = [
+  {
+    url: '/~offline',
+    matcher: ({ request }) => request.destination === 'document',
+  },
+] satisfies PrecacheFallbackEntry[];
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
+  fallbacks: { entries: fallbackEntries },
 });
+
+serwist.addToPrecacheList([{ url: '/~offline' }]);
 
 serwist.addEventListeners();
