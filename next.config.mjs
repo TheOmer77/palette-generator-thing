7bb5e2ef55bbc8c018d454226a132dd228efccbf
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-23 14:55:24 +0300

    Remove `@ducanh2912/next-pwa`

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
-//@ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({ dest: 'public' });
-const config = withPWA();
-export default config;
+export default nextConfig;
