e1218c6d333f64080865440aca0089a3562fa798
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-07-24 17:26:23 +0300

    Add some redirection logic in service worker

--- src/sw.ts
+++ src/sw.ts
@@ -1,17 +1,18 @@
 import {
   Serwist,
+  Strategy,
   type PrecacheEntry,
   type PrecacheFallbackEntry,
   type SerwistGlobalConfig,
 } from 'serwist';
 import { defaultCache } from '@serwist/next/worker';
+import { validateSearchParams } from './lib/validateSearchParams';
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
   }
 }
-
 declare const self: ServiceWorkerGlobalScope;
 
 const fallbackEntries = [
@@ -21,12 +22,33 @@ const fallbackEntries = [
   },
 ] satisfies PrecacheFallbackEntry[];
 
+class RedirectToValidSearch extends Strategy {
+  _handle(request: Request) {
+    const { pathname, searchParams } = new URL(request.url);
+    // isValid should be false if this is called
+    const { validSearch } = validateSearchParams(searchParams);
+    return new Promise<Response>(resolve =>
+      resolve(Response.redirect(`${pathname}?${validSearch}`))
+    );
+  }
+}
+
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+    {
+      matcher: ({ request }) => {
+        const { searchParams } = new URL(request.url);
+        const { isValid } = validateSearchParams(searchParams);
+        return request.destination === 'document' && !isValid;
+      },
+      handler: new RedirectToValidSearch(),
+    },
+    ...defaultCache,
+  ],
   fallbacks: { entries: fallbackEntries },
 });
 
