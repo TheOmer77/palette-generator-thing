de589e4dc5be666134a15f85c7703452ef51b89a
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-21 14:06:16 +0300

    Remove unnecessary destructuring of Drawer props

--- src/components/ui/Drawer/Drawer.tsx
+++ src/components/ui/Drawer/Drawer.tsx
@@ -5,7 +5,7 @@ import { Drawer as DrawerPrimitive } from 'vaul';
 
 export type DrawerProps = ComponentPropsWithoutRef<typeof DrawerPrimitive.Root>;
 
-export const Drawer = ({ ...props }: DrawerProps) => (
+export const Drawer = (props: DrawerProps) => (
   <DrawerPrimitive.Root {...props} shouldScaleBackground={false} />
 );
 Drawer.displayName = 'Drawer';
