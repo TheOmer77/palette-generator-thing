8cdc436309a307ad58da61c77b9b9514c6ccc83b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-08-21 13:39:29 +0300

    Reorder imports in useBreakpoint

--- src/hooks/useBreakpoint.ts
+++ src/hooks/useBreakpoint.ts
@@ -1,5 +1,6 @@
-import { screens } from '@/config/tailwind';
 import { useMediaQuery } from 'usehooks-ts';
+
+import { screens } from '@/config/tailwind';
 
 export const useBreakpoint = (breakpoint: keyof typeof screens) =>
   useMediaQuery(`(min-width: ${screens[breakpoint]})`);
